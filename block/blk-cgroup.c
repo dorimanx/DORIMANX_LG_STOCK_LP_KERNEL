d4744e1126f640a6266fc1c0712d59e5c3549704
Tejun Heo <tj@kernel.org>
2012-03-05 13:15:04 -0800

    blkcg: add blkio_policy[] array and allow one policy per policy ID
    Block cgroup policies are maintained in a linked list and,
    theoretically, multiple policies sharing the same policy ID are
    allowed.
    
    This patch temporarily restricts one policy per plid and adds
    blkio_policy[] array which indexes registered policy types by plid.
    Both the restriction and blkio_policy[] array are transitional and
    will be removed once API cleanup is complete.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -30,6 +30,8 @@ static LIST_HEAD(blkio_list);
 struct blkio_cgroup blkio_root_cgroup = { .weight = 2*BLKIO_WEIGHT_DEFAULT };
 EXPORT_SYMBOL_GPL(blkio_root_cgroup);
 
+static struct blkio_policy_type *blkio_policy[BLKIO_NR_POLICIES];
+
 /* for encoding cft->private value on file */
 #define BLKIOFILE_PRIVATE(x, val)	(((x) << 16) | (val))
 /* What policy owns the file, proportional or throttle */
@@ -1843,7 +1845,11 @@ EXPORT_SYMBOL_GPL(blkio_subsys);
 void blkio_policy_register(struct blkio_policy_type *blkiop)
 {
 	spin_lock(&blkio_list_lock);
+
+	BUG_ON(blkio_policy[blkiop->plid]);
+	blkio_policy[blkiop->plid] = blkiop;
 	list_add_tail(&blkiop->list, &blkio_list);
+
 	spin_unlock(&blkio_list_lock);
 }
 EXPORT_SYMBOL_GPL(blkio_policy_register);
@@ -1851,7 +1857,11 @@ EXPORT_SYMBOL_GPL(blkio_policy_register);
 void blkio_policy_unregister(struct blkio_policy_type *blkiop)
 {
 	spin_lock(&blkio_list_lock);
+
+	BUG_ON(blkio_policy[blkiop->plid] != blkiop);
+	blkio_policy[blkiop->plid] = NULL;
 	list_del_init(&blkiop->list);
+
 	spin_unlock(&blkio_list_lock);
 }
 EXPORT_SYMBOL_GPL(blkio_policy_unregister);
