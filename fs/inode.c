e68ad846d641806b8539d0f37c5b813edc69c366
Randy Dunlap <rdunlap@xenotime.net>
2012-04-17 17:03:25 -0700

    fs: fix inode.c kernel-doc warnings
    Fix kernel-doc warnings in fs/inode.c:
    
    Warning(fs/inode.c:1493): No description found for parameter 'path'
    Warning(fs/inode.c:1493): Excess function parameter 'mnt' description in 'touch_atime'
    Warning(fs/inode.c:1493): Excess function parameter 'dentry' description in 'touch_atime'
    
    Signed-off-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/inode.c
+++ fs/inode.c
@@ -1522,8 +1522,7 @@ static int update_time(struct inode *inode, struct timespec *time, int flags)
 
 /**
  *	touch_atime	-	update the access time
- *	@mnt: mount the inode is accessed on
- *	@dentry: dentry accessed
+ *	@path: the &struct path to update
  *
  *	Update the accessed time on an inode and mark it for writeback.
  *	This function automatically handles read only file systems and media,
