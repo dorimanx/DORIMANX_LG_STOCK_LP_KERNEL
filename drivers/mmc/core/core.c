aeee06891bfdfc39d8d05de7828f84097795a490
Ulf Hansson <ulf.hansson@linaro.org>
2012-05-09 16:15:26 +0200

    mmc: core: Prevent eMMC VCC supply to be cut from late init
    For eMMC cards that has been initialized from a bootloader,
    the VCC voltage supply must not be cut in an uncontrolled
    manner, without first sending SLEEP or POWEROFF_NOTIFY.
    
    The regulator_init_complete late initcall, may cut the VCC
    regulator if it's reference counter is zero. To be able to
    prevent the regulator from being cut, mmc_start_host, which
    should execute at device init and thus before late init,
    calls mmc_power_up. Then the host driver is able to increase
    the reference to the regulator.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Reviewed-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -63,6 +63,7 @@ static void mmc_clk_scaling(struct mmc_host *host, bool from_wq);
 #define MMC_FLUSH_REQ_TIMEOUT_MS 180000 /* msec */
 
 static struct workqueue_struct *workqueue;
+static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
 
 /*
  * Enabling software CRCs on the data blocks can be a significant (30%)
@@ -2005,6 +2006,9 @@ void mmc_power_up(struct mmc_host *host)
 {
 	int bit;
 
+	if (host->ios.power_mode == MMC_POWER_ON)
+		return;
+
 	mmc_host_clk_hold(host);
 
 	/* If ocr is set, we use it */
@@ -2063,16 +2067,8 @@ void mmc_power_up(struct mmc_host *host)
 
 void mmc_power_off(struct mmc_host *host)
 {
-	#ifdef CONFIG_MACH_LGE
-		/* LGE_CHANGE
-		 * If it is already power-off, skip below.
-		 * 2014-01-16, B2-BSP-FS@lge.com
-		 */
-		if (host->ios.power_mode == MMC_POWER_OFF) {
-            pr_info("[LGE][MMC][%-18s( )] host->index:%d, already power-off, skip below\n", __func__, host->index);
-			return;
-		}
-	#endif
+	if (host->ios.power_mode == MMC_POWER_OFF)
+		return;
 
 	mmc_host_clk_hold(host);
 
@@ -3428,7 +3424,8 @@ void mmc_rescan(struct work_struct *work)
 
 void mmc_start_host(struct mmc_host *host)
 {
-	mmc_power_off(host);
+	host->f_init = max(freqs[0], host->f_min);
+	mmc_power_up(host);
 	mmc_detect_change(host, 0);
 }
 
