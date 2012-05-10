aea62f5808f6f2ad63ea80ad3e47401b543726e4
Linus Torvalds <torvalds@linux-foundation.org>
2012-05-10 12:37:10 -0700

    vfs: move dentry name length comparison from dentry_cmp() into callers
    All callers do want to check the dentry length, but some of them can
    check the length and the hash together, so doing it in dentry_cmp() can
    be counter-productive.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/dcache.c
+++ fs/dcache.c
@@ -197,9 +197,6 @@ static inline int dentry_string_cmp(const unsigned char *cs, const unsigned char
 
 static inline int dentry_cmp(const struct dentry *dentry, const unsigned char *ct, unsigned tcount)
 {
-	if (dentry->d_name.len != tcount)
-		return 1;
-
 	/*
 	 * Be careful about RCU walk racing with rename:
 	 * use ACCESS_ONCE to fetch the name pointer.
@@ -1486,6 +1483,8 @@ static struct dentry *__d_instantiate_unique(struct dentry *entry,
 			continue;
 		if (alias->d_parent != entry->d_parent)
 			continue;
+		if (alias->d_name.len != len)
+			continue;
 		if (dentry_cmp(alias, name, len))
 			continue;
 		__dget(alias);
@@ -1906,6 +1905,8 @@ seqretry:
 			}
 		}
 
+		if (dentry->d_name.len != len)
+			continue;
 		if (!dentry_cmp(dentry, str, len))
 			return dentry;
 	}
@@ -2008,6 +2009,8 @@ struct dentry *__d_lookup(struct dentry *parent, struct qstr *name)
 						tlen, tname, name))
 				goto next;
 		} else {
+			if (dentry->d_name.len != len)
+				goto next;
 			if (dentry_cmp(dentry, str, len))
 				goto next;
 		}
