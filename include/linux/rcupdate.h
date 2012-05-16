e4172cc6865fef6ef3e356f69c402ae4491dcb3f
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2012-05-16 15:42:30 -0700

    rcu: Remove return value from RCU_INIT_POINTER()
    The return value from RCU_INIT_POINTER() is not used, and using it
    would be quite ugly, for example:
    
    	q = RCU_INIT_POINTER(global_p, p);
    
    To prevent this sort of ugliness from appearing, this commit wraps
    RCU_INIT_POINTER() in a do-while loop.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Acked-by: David Howells <dhowells@redhat.com>

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -953,7 +953,9 @@ static inline notrace void rcu_read_unlock_sched_notrace(void)
  * the reader-accessible portions of the linked structure.
  */
 #define RCU_INIT_POINTER(p, v) \
-		p = (typeof(*v) __force __rcu *)(v)
+	do { \
+		p = (typeof(*v) __force __rcu *)(v); \
+	} while (0)
 
 static __always_inline bool __is_kfree_rcu_offset(unsigned long offset)
 {
