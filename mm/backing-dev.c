38a0703ec6cf134ed42bfaf78a436a5a178359d9
Jan Kara <jack@suse.cz>
2012-05-24 18:59:11 +0200

    block: Convert BDI proportion calculations to flexible proportions
    Convert calculations of proportion of writeback each bdi does to new flexible
    proportion code. That allows us to use aging period of fixed wallclock time
    which gives better proportion estimates given the hugely varying throughput of
    different devices.
    
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- mm/backing-dev.c
+++ mm/backing-dev.c
@@ -679,7 +679,7 @@ int bdi_init(struct backing_dev_info *bdi)
 
 	bdi->min_ratio = 0;
 	bdi->max_ratio = 100;
-	bdi->max_prop_frac = PROP_FRAC_BASE;
+	bdi->max_prop_frac = FPROP_FRAC_BASE;
 	spin_lock_init(&bdi->wb_lock);
 	INIT_LIST_HEAD(&bdi->bdi_list);
 	INIT_LIST_HEAD(&bdi->work_list);
@@ -702,7 +702,7 @@ int bdi_init(struct backing_dev_info *bdi)
 	bdi->write_bandwidth = INIT_BW;
 	bdi->avg_write_bandwidth = INIT_BW;
 
-	err = prop_local_init_percpu(&bdi->completions);
+	err = fprop_local_init_percpu(&bdi->completions);
 
 	if (err) {
 err:
@@ -746,7 +746,7 @@ void bdi_destroy(struct backing_dev_info *bdi)
 	for (i = 0; i < NR_BDI_STAT_ITEMS; i++)
 		percpu_counter_destroy(&bdi->bdi_stat[i]);
 
-	prop_local_destroy_percpu(&bdi->completions);
+	fprop_local_destroy_percpu(&bdi->completions);
 }
 EXPORT_SYMBOL(bdi_destroy);
 
