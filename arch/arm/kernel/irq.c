3f3a39cce3495e2d718bd2aba4dfdc3cb3299c0b
Ludovic Desroches <ludovic.desroches@atmel.com>
2012-05-25 14:11:51 +0200

    ARM: at91: aic can use fast eoi handler type
    The Advanced Interrupt Controller allows us to use the fast EOI handler type.
    It lets us remove the Atmel specific workaround into arch/arm/kernel/irq.c
    used to indicate to the AIC the end of the interrupt treatment.
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@atmel.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/kernel/irq.c
+++ arch/arm/kernel/irq.c
@@ -43,13 +43,6 @@
 
 #include <asm/perftypes.h>
 
-/*
- * No architecture-specific irq_finish function defined in arm/arch/irqs.h.
- */
-#ifndef irq_finish
-#define irq_finish(irq) do { } while (0)
-#endif
-
 unsigned long irq_err_count;
 
 int arch_show_interrupts(struct seq_file *p, int prec)
@@ -88,9 +81,6 @@ void handle_IRQ(unsigned int irq, struct pt_regs *regs)
 	} else {
 		generic_handle_irq(irq);
 	}
-
-	/* AT91 specific workaround */
-	irq_finish(irq);
 
 	irq_exit();
 	set_irq_regs(old_regs);
