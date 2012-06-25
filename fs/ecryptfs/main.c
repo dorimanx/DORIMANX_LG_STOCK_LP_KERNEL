3e30f8dcf2f66b81d074622568d79080fdd2b95b
Al Viro <viro@zeniv.linux.org.uk>
2012-06-25 11:38:56 +0400

    ecryptfs: don't reinvent the wheels, please - use struct completion
    ... and keep the sodding requests on stack - they are small enough.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ecryptfs/main.c
+++ fs/ecryptfs/main.c
@@ -720,11 +720,6 @@ static struct ecryptfs_cache_info {
 		.name = "ecryptfs_key_tfm_cache",
 		.size = sizeof(struct ecryptfs_key_tfm),
 	},
-	{
-		.cache = &ecryptfs_open_req_cache,
-		.name = "ecryptfs_open_req_cache",
-		.size = sizeof(struct ecryptfs_open_req),
-	},
 #ifdef CONFIG_CRYPTO_DEV_KFIPS
 	{
 		.cache = &ecryptfs_page_crypt_req_cache,
