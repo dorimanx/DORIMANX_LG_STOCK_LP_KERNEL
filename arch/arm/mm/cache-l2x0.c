6755118b0d7ddfa529236e0bbec4b68b6c5fb56e
Kirill Artamonov <kartamonov@nvidia.com>
2012-07-16 17:24:00 +0300

    ARM: mm: cache-l2x0: Implement outer_clean_all()
    There is already implemented full outer clean routine in
    arch/arm/mm/cache-l2x0.c.
    
    Make it possible to use it through outer_cache interface,
    like other outer maintenance functions.
    
    bug 983964
    
    Change-Id: I47f1fad536c151c255e6a42d6517114c334ddfef
    Reviewed-on: http://git-master/r/116074
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Kirill Artamonov <kartamonov@nvidia.com>
    Tested-by: Kirill Artamonov <kartamonov@nvidia.com>
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: Justin Paver <jpaver@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    
    Rebase-Id: Rf769958fea9fd10058a4805eeafe08f5f0e895dd
    
    Conflicts:
    	arch/arm/mm/cache-l2x0.c

--- arch/arm/mm/cache-l2x0.c
+++ arch/arm/mm/cache-l2x0.c
@@ -430,6 +430,7 @@ void __init l2x0_init(void __iomem *base, u32 aux_val, u32 aux_mask)
 	outer_cache.flush_range = l2x0_flush_range;
 	outer_cache.sync = l2x0_cache_sync;
 	outer_cache.flush_all = l2x0_flush_all;
+	outer_cache.clean_all = l2x0_clean_all;
 	outer_cache.inv_all = l2x0_inv_all;
 	outer_cache.disable = l2x0_disable;
 
