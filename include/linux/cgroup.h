d724be97176c2c96c88fb6618d1971a8ae607770
Tejun Heo <tj@kernel.org>
2012-09-05 12:20:58 -0700

    cgroup: kill cgroup_subsys->__DEPRECATED_clear_css_refs
    2ef37d3fe4 ("memcg: Simplify mem_cgroup_force_empty_list error
    handling") removed the last user of __DEPRECATED_clear_css_refs.  This
    patch removes __DEPRECATED_clear_css_refs and mechanisms to support
    it.
    
    * Conditionals dependent on __DEPRECATED_clear_css_refs removed.
    
    * cgroup_clear_css_refs() can no longer fail.  All that needs to be
      done are deactivating refcnts, setting CSS_REMOVED and putting the
      base reference on each css.  Remove cgroup_clear_css_refs() and the
      failure path, and open-code the loops into cgroup_rmdir().
    
    This patch keeps the two for_each_subsys() loops separate while open
    coding them.  They can be merged now but there are scheduled changes
    which need them to be separate, so keep them separate to reduce the
    amount of churn.
    
    local_irq_save/restore() from cgroup_clear_css_refs() are replaced
    with local_irq_disable/enable() for simplicity.  This is safe as
    cgroup_rmdir() is always called with IRQ enabled.  Note that this IRQ
    switching is necessary to ensure that css_tryget() isn't called from
    IRQ context on the same CPU while lower context is between CSS
    deactivation and setting CSS_REMOVED as css_tryget() would hang
    forever in such cases waiting for CSS to be re-activated or
    CSS_REMOVED set.  This will go away soon.
    
    v2: cgroup_call_pre_destroy() removal dropped per Michal.  Commit
        message updated to explain local_irq_disable/enable() conversion.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: Michal Hocko <mhocko@suse.cz>
    Reviewed-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Acked-by: Li Zefan <lizefan@huawei.com>

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -88,7 +88,6 @@ struct cgroup_subsys_state {
 enum {
 	CSS_ROOT, /* This CSS is the root of the subsystem */
 	CSS_REMOVED, /* This CSS is dead */
-	CSS_CLEAR_CSS_REFS,		/* @ss->__DEPRECATED_clear_css_refs */
 };
 
 /*
@@ -486,17 +485,6 @@ struct cgroup_subsys {
 	 * (not available in early_init time.)
 	 */
 	bool use_id;
-
-	/*
-	 * If %true, cgroup removal will try to clear css refs by retrying
-	 * ss->pre_destroy() until there's no css ref left.  This behavior
-	 * is strictly for backward compatibility and will be removed as
-	 * soon as the current user (memcg) is updated.
-	 *
-	 * If %false, ss->pre_destroy() can't fail and cgroup removal won't
-	 * wait for css refs to drop to zero before proceeding.
-	 */
-	bool __DEPRECATED_clear_css_refs;
 
 	/*
 	 * If %false, this subsystem is properly hierarchical -
