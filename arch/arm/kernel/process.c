d182f6a450dc2b3ee65079cdf95163ddb272f965
Stephen Boyd <sboyd@codeaurora.org>
2012-09-05 11:54:32 -0700

    ARM: Fix deadlock scenario with smp_send_stop()
    If one process calls sys_reboot and that process then stops other
    CPUs while those CPUs are within a spin_lock() region we can
    potentially encounter a deadlock scenario like below.
    
    CPU 0                   CPU 1
    -----                   -----
                            spin_lock(my_lock)
    smp_send_stop()
     send_IPI               handle_IPI()
                             disable_preemption/irqs
                              while(1);
     <PREEMPT>
    spin_lock(my_lock) <--- Waits forever
    
    We shouldn't attempt to run any other tasks after we send a stop
    IPI to a CPU so disable preemption so that this task runs to
    completion.
    
    Change-Id: I528af0b4448433bd9d6ca12d6a0e2c422a55d4be
    CRs-Fixed: 396169
    Reported-by: Sundarajan Srinivasan <sundaraj@codeaurora.com>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -273,6 +273,7 @@ void machine_halt(void)
  */
 void machine_power_off(void)
 {
+	preempt_disable();
 	smp_send_stop();
 
 	if (pm_power_off)
@@ -292,6 +293,7 @@ void machine_power_off(void)
  */
 void machine_restart(char *cmd)
 {
+	preempt_disable();
 	smp_send_stop();
 
 	/* Flush the console to make sure all the relevant messages make it
