981b18225a222c59277cb323748bb3309dfbd25e
Daniel Vetter <daniel.vetter@ffwll.ch>
2012-09-17 23:03:31 +0000

    console: use might_sleep in console_lock
    Instead of BUG_ON(in_interrupt()), since that doesn't check for all
    the newfangled stuff like preempt.
    
    Note that this is valid since the console_sem is essentially used like
    a real mutex with only two twists:
    - we allow trylock from hardirq context
    - across suspend/resume we lock the logical console_lock, but drop the
      semaphore protecting the locking state.
    
    Now that doesn't guarantee that no one is playing tricks in
    single-thread atomic contexts at suspend/resume/boot time, but
    - I couldn't find anything suspicious with some grepping,
    - might_sleep shouldn't die,
    - and I think the upside of catching more potential issues is worth
      the risk of getting a might_sleep backtrace that would have been
      save (and then dealing with that fallout).
    
    Cc: Dave Airlie <airlied@gmail.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/printk.c
+++ kernel/printk.c
@@ -1935,7 +1935,8 @@ static int __cpuinit console_cpu_notify(struct notifier_block *self,
  */
 void console_lock(void)
 {
-	BUG_ON(in_interrupt());
+	might_sleep();
+
 	down(&console_sem);
 	if (console_suspended)
 		return;
