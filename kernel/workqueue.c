0f832c234403a301a4bbea773aec62031303825c
Tejun Heo <tj@kernel.org>
2012-09-20 10:03:19 -0700

    workqueue: remove spurious WARN_ON_ONCE(in_irq()) from try_to_grab_pending()
    e0aecdd874 ("workqueue: use irqsafe timer for delayed_work") made
    try_to_grab_pending() safe to use from irq context but forgot to
    remove WARN_ON_ONCE(in_irq()).  Remove it.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1057,8 +1057,6 @@ static int try_to_grab_pending(struct work_struct *work, bool is_dwork,
 {
 	struct global_cwq *gcwq;
 
-	WARN_ON_ONCE(in_irq());
-
 	local_irq_save(*flags);
 
 	/* try to steal the timer if it exists */
