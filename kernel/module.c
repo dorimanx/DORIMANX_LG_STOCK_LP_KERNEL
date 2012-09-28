1e87f093c9aec62cf5db0876bef8df6da7967f69
Rusty Russell <rusty@rustcorp.com.au>
2012-09-28 14:31:03 +0930

    module: fix symbol waiting when module fails before init
    We use resolve_symbol_wait(), which blocks if the module containing
    the symbol is still loading.  However:
    
    1) The module_wq we use is only woken after calling the modules' init
       function, but there are other failure paths after the module is
       placed in the linked list where we need to do the same thing.
    
    2) wake_up() only wakes one waiter, and our waitqueue is shared by all
       modules, so we need to wake them all.
    
    3) wake_up_all() doesn't imply a memory barrier: I feel happier calling
       it after we've grabbed and dropped the module_mutex, not just after
       the state assignment.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- kernel/module.c
+++ kernel/module.c
@@ -2979,7 +2979,7 @@ static struct module *load_module(void __user *umod,
 	/* Unlink carefully: kallsyms could be walking list. */
 	list_del_rcu(&mod->list);
 	module_bug_cleanup(mod);
-
+	wake_up_all(&module_wq);
  ddebug:
 	dynamic_debug_remove(info.debug);
  unlock:
@@ -3054,7 +3054,7 @@ SYSCALL_DEFINE3(init_module, void __user *, umod,
 		blocking_notifier_call_chain(&module_notify_list,
 					     MODULE_STATE_GOING, mod);
 		free_module(mod);
-		wake_up(&module_wq);
+		wake_up_all(&module_wq);
 		return ret;
 	}
 	if (ret > 0) {
@@ -3066,9 +3066,8 @@ SYSCALL_DEFINE3(init_module, void __user *, umod,
 		dump_stack();
 	}
 
-	/* Now it's a first class citizen!  Wake up anyone waiting for it. */
+	/* Now it's a first class citizen! */
 	mod->state = MODULE_STATE_LIVE;
-	wake_up(&module_wq);
 	blocking_notifier_call_chain(&module_notify_list,
 				     MODULE_STATE_LIVE, mod);
 
@@ -3091,6 +3090,7 @@ SYSCALL_DEFINE3(init_module, void __user *, umod,
 	mod->init_ro_size = 0;
 	mod->init_text_size = 0;
 	mutex_unlock(&module_mutex);
+	wake_up_all(&module_wq);
 
 	return 0;
 }
