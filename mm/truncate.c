8b2aef989144f6e863734727f950cc318edcb31f
Hugh Dickins <hughd@google.com>
2012-10-08 16:33:19 -0700

    mm: use clear_page_mlock() in page_remove_rmap()
    We had thought that pages could no longer get freed while still marked as
    mlocked; but Johannes Weiner posted this program to demonstrate that
    truncating an mlocked private file mapping containing COWed pages is still
    mishandled:
    
    #include <sys/types.h>
    #include <sys/mman.h>
    #include <sys/stat.h>
    #include <stdlib.h>
    #include <unistd.h>
    #include <fcntl.h>
    #include <stdio.h>
    
    int main(void)
    {
    	char *map;
    	int fd;
    
    	system("grep mlockfreed /proc/vmstat");
    	fd = open("chigurh", O_CREAT|O_EXCL|O_RDWR);
    	unlink("chigurh");
    	ftruncate(fd, 4096);
    	map = mmap(NULL, 4096, PROT_WRITE, MAP_PRIVATE, fd, 0);
    	map[0] = 11;
    	mlock(map, sizeof(fd));
    	ftruncate(fd, 0);
    	close(fd);
    	munlock(map, sizeof(fd));
    	munmap(map, 4096);
    	system("grep mlockfreed /proc/vmstat");
    	return 0;
    }
    
    The anon COWed pages are not caught by truncation's clear_page_mlock() of
    the pagecache pages; but unmap_mapping_range() unmaps them, so we ought to
    look out for them there in page_remove_rmap().  Indeed, why should
    truncation or invalidation be doing the clear_page_mlock() when removing
    from pagecache?  mlock is a property of mapping in userspace, not a
    property of pagecache: an mlocked unmapped page is nonsensical.
    
    Reported-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Hugh Dickins <hughd@google.com>
    Cc: Mel Gorman <mel@csn.ul.ie>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Michel Lespinasse <walken@google.com>
    Cc: Ying Han <yinghan@google.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/truncate.c
+++ mm/truncate.c
@@ -108,7 +108,6 @@ truncate_complete_page(struct address_space *mapping, struct page *page)
 
 	cancel_dirty_page(page, PAGE_CACHE_SIZE);
 
-	clear_page_mlock(page);
 	ClearPageMappedToDisk(page);
 	delete_from_page_cache(page);
 	return 0;
@@ -133,7 +132,6 @@ invalidate_complete_page(struct address_space *mapping, struct page *page)
 	if (page_has_private(page) && !try_to_release_page(page, 0))
 		return 0;
 
-	clear_page_mlock(page);
 	ret = remove_mapping(mapping, page);
 
 	return ret;
@@ -394,8 +392,6 @@ invalidate_complete_page2(struct address_space *mapping, struct page *page)
 
 	if (page_has_private(page) && !try_to_release_page(page, GFP_KERNEL))
 		return 0;
-
-	clear_page_mlock(page);
 
 	spin_lock_irq(&mapping->tree_lock);
 	if (PageDirty(page))
