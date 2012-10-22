e5d0e6cdc5c4d525f00aff4e706e70932d407c91
Eric Dumazet <edumazet@google.com>
2012-10-22 09:03:40 +0000

    net: fix secpath kmemleak
    Mike Kazantsev found 3.5 kernels and beyond were leaking memory,
    and tracked the faulty commit to a1c7fff7e18f59e ("net:
    netdev_alloc_skb() use build_skb()")
    
    While this commit seems fine, it uncovered a bug introduced
    in commit bad43ca8325 ("net: introduce skb_try_coalesce()), in function
    kfree_skb_partial()"):
    
    If head is stolen, we free the sk_buff,
    without removing references on secpath (skb->sp).
    
    So IPsec + IP defrag/reassembly (using skb coalescing), or
    TCP coalescing could leak secpath objects.
    
    Fix this bug by calling skb_release_head_state(skb) to properly
    release all possible references to linked objects.
    
    Reported-by: Mike Kazantsev <mk.fraggod@gmail.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Bisected-by: Mike Kazantsev <mk.fraggod@gmail.com>
    Tested-by: Mike Kazantsev <mk.fraggod@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -3478,10 +3478,12 @@ EXPORT_SYMBOL_GPL(skb_gso_transport_seglen);
 
 void kfree_skb_partial(struct sk_buff *skb, bool head_stolen)
 {
-	if (head_stolen)
+	if (head_stolen) {
+		skb_release_head_state(skb);
 		kmem_cache_free(skbuff_head_cache, skb);
-	else
+	} else {
 		__kfree_skb(skb);
+	}
 }
 EXPORT_SYMBOL(kfree_skb_partial);
 
