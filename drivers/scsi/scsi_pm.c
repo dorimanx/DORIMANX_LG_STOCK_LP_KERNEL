43eb86a79b30db73699e90c1edd29c769c1230b7
Aaron Lu <aaron.lu@intel.com>
2012-11-09 15:27:53 +0800

    [SCSI] Revert "[SCSI] runtime resume parent for child's system-resume"
    This reverts commit 28fd00d42cca178638f51c08efa986a777c24a4b.
    
    With commit 88d26136a256576e444db312179e17af6dd0ea87 (PM: Prevent
    runtime suspend during system resume), this patch is no longer needed.
    
    Signed-off-by: Aaron Lu <aaron.lu@intel.com>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Acked-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/scsi_pm.c
+++ drivers/scsi/scsi_pm.c
@@ -70,17 +70,8 @@ scsi_bus_resume_common(struct device *dev, int (*cb)(struct device *))
 {
 	int err = 0;
 
-	if (scsi_is_sdev_device(dev)) {
-		/*
-		 * Parent device may have runtime suspended as soon as
-		 * it is woken up during the system resume.
-		 *
-		 * Resume it on behalf of child.
-		 */
-		pm_runtime_get_sync(dev->parent);
+	if (scsi_is_sdev_device(dev))
 		err = scsi_dev_type_resume(dev, cb);
-		pm_runtime_put_sync(dev->parent);
-	}
 
 	if (err == 0) {
 		pm_runtime_disable(dev);
