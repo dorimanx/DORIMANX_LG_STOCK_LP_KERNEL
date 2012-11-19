6ca176067e36c6de17eb9a754fa82dbe515080a9
Tejun Heo <tj@kernel.org>
2012-11-19 08:13:37 -0800

    cgroup: introduce CSS_ONLINE flag and on/offline_css() helpers
    New helpers on/offline_css() respectively wrap ->post_create() and
    ->pre_destroy() invocations.  online_css() sets CSS_ONLINE after
    ->post_create() is complete and offline_css() invokes ->pre_destroy()
    iff CSS_ONLINE is set and clears it while also handling the temporary
    dropping of cgroup_mutex.
    
    This patch doesn't introduce any behavior change at the moment but
    will be used to improve cgroup_create() failure path and allow
    ->post_create() to fail.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: Li Zefan <lizefan@huawei.com>

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -88,6 +88,7 @@ struct cgroup_subsys_state {
 /* bits in struct cgroup_subsys_state flags field */
 enum {
 	CSS_ROOT	= (1 << 0), /* this CSS is the root of the subsystem */
+	CSS_ONLINE	= (1 << 1), /* between ->post_create() and ->pre_destroy() */
 };
 
 /*
