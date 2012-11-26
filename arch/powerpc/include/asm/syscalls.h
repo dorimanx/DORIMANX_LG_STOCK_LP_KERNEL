6364e39671c0e88d03fd7ed6051a781da62f17a8
Al Viro <viro@zeniv.linux.org.uk>
2012-11-25 23:12:10 -0500

    sanitize rt_sigaction() situation a bit
    	Switch from __ARCH_WANT_SYS_RT_SIGACTION to opposite
    (!CONFIG_ODD_RT_SIGACTION); the only two architectures that
    need it are alpha and sparc.  The reason for use of CONFIG_...
    instead of __ARCH_... is that it's needed only kernel-side
    and doing it that way avoids a mess with include order on many
    architectures.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/powerpc/include/asm/syscalls.h
+++ arch/powerpc/include/asm/syscalls.h
@@ -31,9 +31,6 @@ asmlinkage int sys_vfork(unsigned long p1, unsigned long p2,
 		unsigned long p6, struct pt_regs *regs);
 asmlinkage long sys_pipe(int __user *fildes);
 asmlinkage long sys_pipe2(int __user *fildes, int flags);
-asmlinkage long sys_rt_sigaction(int sig,
-		const struct sigaction __user *act,
-		struct sigaction __user *oact, size_t sigsetsize);
 asmlinkage long ppc64_personality(unsigned long personality);
 asmlinkage int ppc_rtas(struct rtas_args __user *uargs);
 asmlinkage time_t sys64_time(time_t __user * tloc);
