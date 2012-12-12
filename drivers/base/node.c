13f0c15c18a9818a4af2d4a880330a425dd1fad7
Lai Jiangshan <laijs@cn.fujitsu.com>
2012-12-12 13:52:00 -0800

    numa: add CONFIG_MOVABLE_NODE for movable-dedicated node
    We need a node which only contains movable memory.  This feature is very
    important for node hotplug.  If a node has normal/highmem, the memory may
    be used by the kernel and can't be offlined.  If the node only contains
    movable memory, we can offline the memory and the node.
    
    All are prepared, we can actually introduce N_MEMORY.
    add CONFIG_MOVABLE_NODE make we can use it for movable-dedicated node
    
    [akpm@linux-foundation.org: fix Kconfig text]
    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    Tested-by: Yasuaki Ishimatsu <isimatu.yasuaki@jp.fujitsu.com>
    Signed-off-by: Wen Congyang <wency@cn.fujitsu.com>
    Cc: Jiang Liu <jiang.liu@huawei.com>
    Cc: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Cc: Minchan Kim <minchan.kim@gmail.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Yinghai Lu <yinghai@kernel.org>
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Cc: Greg KH <greg@kroah.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/base/node.c
+++ drivers/base/node.c
@@ -623,6 +623,9 @@ static struct node_attr node_state_attr[] = {
 #ifdef CONFIG_HIGHMEM
 	_NODE_ATTR(has_high_memory, N_HIGH_MEMORY),
 #endif
+#ifdef CONFIG_MOVABLE_NODE
+	[N_MEMORY] = _NODE_ATTR(has_memory, N_MEMORY),
+#endif
 };
 
 static struct attribute *node_state_attrs[] = {
@@ -632,6 +635,9 @@ static struct attribute *node_state_attrs[] = {
 	&node_state_attr[3].attr.attr,
 #ifdef CONFIG_HIGHMEM
 	&node_state_attr[4].attr.attr,
+#endif
+#ifdef CONFIG_MOVABLE_NODE
+	&node_state_attr[N_MEMORY].attr.attr,
 #endif
 	NULL
 };
