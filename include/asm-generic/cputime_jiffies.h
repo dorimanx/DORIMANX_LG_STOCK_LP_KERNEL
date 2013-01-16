f45edea0053b1becb97aecbf312d639feedfc392
Frederic Weisbecker <fweisbec@gmail.com>
2013-01-16 18:02:04 +0100

    cputime: Move default nsecs_to_cputime() to jiffies based cputime file
    If the architecture doesn't provide an implementation of
    nsecs_to_cputime(), the cputime accounting core uses a
    default one that converts the nanoseconds to jiffies. However
    this only makes sense if we use the jiffies based cputime.
    
    For now it doesn't matter much because this API is only
    called on code that uses jiffies based cputime accounting.
    
    But the code may evolve and this API may be used more
    broadly in the future. Keeping this default implementation
    around is very error prone as it may introduce a bug and
    hide it on architectures that don't override this API.
    
    Fix this by moving this definition to the jiffies based
    cputime headers as it is the only place where it belongs to.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Li Zhong <zhong@linux.vnet.ibm.com>
    Cc: Namhyung Kim <namhyung.kim@lge.com>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Paul Gortmaker <paul.gortmaker@windriver.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/asm-generic/cputime_jiffies.h
+++ include/asm-generic/cputime_jiffies.h
@@ -14,8 +14,14 @@ typedef u64 __nocast cputime64_t;
 #define jiffies64_to_cputime64(__jif)	(__force cputime64_t)(__jif)
 #define cputime64_sub(__a, __b)		((__a) - (__b))
 
-#define nsecs_to_cputime64(__ct)	\
-	jiffies64_to_cputime64(nsecs_to_jiffies64(__ct))
+
+/*
+ * Convert nanoseconds to cputime
+ */
+#define nsecs_to_cputime64(__nsec)	\
+	jiffies64_to_cputime64(nsecs_to_jiffies64(__nsec))
+#define nsecs_to_cputime(__nsec)	\
+	jiffies_to_cputime(nsecs_to_jiffies(__nsec))
 
 
 /*
