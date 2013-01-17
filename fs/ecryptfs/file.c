d452aaf70bcadb9363f6a3dd0979b0931e6cedb8
Tyler Hicks <tyhicks@canonical.com>
2013-01-17 11:36:10 -0800

    eCryptfs: Fix -Wunused-but-set-variable warnings
    These two variables are no longer used and can be removed. Fixes
    warnings when building with W=1.
    
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>

--- fs/ecryptfs/file.c
+++ fs/ecryptfs/file.c
@@ -199,7 +199,6 @@ static int ecryptfs_open(struct inode *inode, struct file *file)
 	struct dentry *ecryptfs_dentry = file->f_path.dentry;
 	/* Private value of ecryptfs_dentry allocated in
 	 * ecryptfs_lookup() */
-	struct dentry *lower_dentry;
 	struct ecryptfs_file_info *file_info;
 
 	/* Released in ecryptfs_release or end of function if failure */
@@ -211,7 +210,6 @@ static int ecryptfs_open(struct inode *inode, struct file *file)
 		rc = -ENOMEM;
 		goto out;
 	}
-	lower_dentry = ecryptfs_dentry_to_lower(ecryptfs_dentry);
 	crypt_stat = &ecryptfs_inode_to_private(inode)->crypt_stat;
 	mutex_lock(&crypt_stat->cs_mutex);
 	if (!(crypt_stat->flags & ECRYPTFS_POLICY_APPLIED)) {
