4ee4d98a95acf840f1c9d48a2dac530c0f2c275f
Tejun Heo <tj@kernel.org>
2013-01-22 16:48:03 -0800

    async, kmod: warn on synchronous request_module() from async workers
    Synchronous requet_module() from an async worker can lead to deadlock
    because module init path may invoke async_synchronize_full().  The
    async worker waits for request_module() to complete and the module
    loading waits for the async task to finish.  This bug happened in the
    block layer because of default elevator auto-loading.
    
    Block layer has been updated not to do default elevator auto-loading
    and it has been decided to disallow synchronous request_module() from
    async workers.
    
    Trigger WARN_ON_ONCE() on synchronous request_module() from async
    workers.
    
    For more details, please refer to the following thread.
    
      http://thread.gmane.org/gmane.linux.kernel/1420814
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reported-by: Alex Riesen <raa.lkml@gmail.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Arjan van de Ven <arjan@linux.intel.com>

--- kernel/kmod.c
+++ kernel/kmod.c
@@ -37,6 +37,8 @@
 #include <linux/notifier.h>
 #include <linux/suspend.h>
 #include <linux/rwsem.h>
+#include <linux/ptrace.h>
+#include <linux/async.h>
 #include <asm/uaccess.h>
 
 #include <trace/events/module.h>
@@ -136,6 +138,14 @@ int __request_module(bool wait, const char *fmt, ...)
 	static atomic_t kmod_concurrent = ATOMIC_INIT(0);
 #define MAX_KMOD_CONCURRENT 50	/* Completely arbitrary value - KAO */
 	static int kmod_loop_msg;
+
+	/*
+	 * We don't allow synchronous module loading from async.  Module
+	 * init may invoke async_synchronize_full() which will end up
+	 * waiting for this task which already is waiting for the module
+	 * loading to complete, leading to a deadlock.
+	 */
+	WARN_ON_ONCE(wait && current_is_async());
 
 	va_start(args, fmt);
 	ret = vsnprintf(module_name, MODULE_NAME_LEN, fmt, args);
