cf7582a0e1a35bcd0d297934b013a7bef75ffeeb
Chris Metcalf <cmetcalf@tilera.com>
2013-02-01 15:04:26 -0500

    genirq: Export enable/disable_percpu_irq()
    These functions are used by the tilegx onchip network driver, and it's
    useful to be able to load that driver as a module.
    
    Signed-off-by: Chris Metcalf <cmetcalf@tilera.com>
    Link: http://lkml.kernel.org/r/201302012043.r11KhNZF024371@farm-0021.internal.tilera.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -1583,6 +1583,7 @@ void enable_percpu_irq(unsigned int irq, unsigned int type)
 out:
 	irq_put_desc_unlock(desc, flags);
 }
+EXPORT_SYMBOL_GPL(enable_percpu_irq);
 
 void disable_percpu_irq(unsigned int irq)
 {
@@ -1596,6 +1597,7 @@ void disable_percpu_irq(unsigned int irq)
 	irq_percpu_disable(desc, cpu);
 	irq_put_desc_unlock(desc, flags);
 }
+EXPORT_SYMBOL_GPL(disable_percpu_irq);
 
 /*
  * Internal function to unregister a percpu irqaction.
