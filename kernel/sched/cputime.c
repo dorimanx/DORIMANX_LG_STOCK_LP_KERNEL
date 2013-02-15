3849dcb7b9a41d990660d3502e170b8b9f507c50
Thomas Gleixner <tglx@linutronix.de>
2013-02-15 23:47:07 +0100

    cputime: Remove irqsave from seqlock readers
    The reader side code has no requirement to disable interrupts while
    sampling data. The sequence counter is enough to ensure consistency.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -775,18 +775,17 @@ void vtime_init_idle(struct task_struct *t)
 
 cputime_t task_gtime(struct task_struct *t)
 {
-	unsigned long flags;
 	unsigned int seq;
 	cputime_t gtime;
 
 	do {
-		seq = read_seqbegin_irqsave(&t->vtime_seqlock, flags);
+		seq = read_seqbegin(&t->vtime_seqlock);
 
 		gtime = t->gtime;
 		if (t->flags & PF_VCPU)
 			gtime += vtime_delta(t);
 
-	} while (read_seqretry_irqrestore(&t->vtime_seqlock, seq, flags));
+	} while (read_seqretry(&t->vtime_seqlock, seq));
 
 	return gtime;
 }
@@ -802,7 +801,6 @@ fetch_task_cputime(struct task_struct *t,
 		   cputime_t *u_src, cputime_t *s_src,
 		   cputime_t *udelta, cputime_t *sdelta)
 {
-	unsigned long flags;
 	unsigned int seq;
 	unsigned long long delta;
 
@@ -810,7 +808,7 @@ fetch_task_cputime(struct task_struct *t,
 		*udelta = 0;
 		*sdelta = 0;
 
-		seq = read_seqbegin_irqsave(&t->vtime_seqlock, flags);
+		seq = read_seqbegin(&t->vtime_seqlock);
 
 		if (u_dst)
 			*u_dst = *u_src;
@@ -834,7 +832,7 @@ fetch_task_cputime(struct task_struct *t,
 			if (t->vtime_snap_whence == VTIME_SYS)
 				*sdelta = delta;
 		}
-	} while (read_seqretry_irqrestore(&t->vtime_seqlock, seq, flags));
+	} while (read_seqretry(&t->vtime_seqlock, seq));
 }
 
 
