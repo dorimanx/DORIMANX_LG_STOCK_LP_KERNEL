a81e751396bd5d2f10e9d92826fffb420a003e47
Sha Zhengju <handai.szj@taobao.com>
2013-02-20 17:14:38 +0800

    sched/core: Remove the obsolete and unused nr_uninterruptible() function
    Signed-off-by: Sha Zhengju <handai.szj@taobao.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -137,7 +137,6 @@ extern int nr_threads;
 DECLARE_PER_CPU(unsigned long, process_counts);
 extern int nr_processes(void);
 extern unsigned long nr_running(void);
-extern unsigned long nr_uninterruptible(void);
 extern unsigned long nr_iowait(void);
 extern unsigned long avg_nr_running(void);
 extern unsigned long nr_iowait_cpu(int cpu);
