2db2853ae3f7c139fb2a9339fa731cc8ae467f3e
Frederic Weisbecker <fweisbec@gmail.com>
2013-02-24 12:59:30 +0100

    context_tracking: Restore preempted context state after preempt_schedule_irq()
    From the context tracking POV, preempt_schedule_irq() behaves pretty much
    like an exception: It can be called anytime and schedule another task.
    
    But currently it doesn't restore the context tracking state of the preempted
    code on preempt_schedule_irq() return.
    
    As a result, if preempt_schedule_irq() is called in the tiny frame between
    user_enter() and the actual return to userspace, we resume userspace with
    the wrong context tracking state.
    
    Fix this by using exception_enter/exit() which are a perfect fit for this
    kind of issue.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Li Zhong <zhong@linux.vnet.ibm.com>
    Cc: Kevin Hilman <khilman@linaro.org>
    Cc: Mats Liljegren <mats.liljegren@enea.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Namhyung Kim <namhyung.kim@lge.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4312,9 +4312,12 @@ EXPORT_SYMBOL(preempt_schedule);
 asmlinkage void __sched preempt_schedule_irq(void)
 {
 	struct thread_info *ti = current_thread_info();
+	enum ctx_state prev_state;
 
 	/* Catch callers which need to be fixed */
 	BUG_ON(ti->preempt_count || !irqs_disabled());
+
+	prev_state = exception_enter();
 
 	do {
 		add_preempt_count(PREEMPT_ACTIVE);
@@ -4329,6 +4332,8 @@ asmlinkage void __sched preempt_schedule_irq(void)
 		 */
 		barrier();
 	} while (need_resched());
+
+	exception_exit(prev_state);
 }
 
 int default_wake_function(wait_queue_t *curr, unsigned mode, int wake_flags,
