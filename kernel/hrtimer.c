95600eb518763efd2137578b0e5e168e148be423
David Daney <ddaney.cavm@gmail.com>
2013-03-13 12:20:38 -0700

    hrtimer/trivial: Fix comment text in hrtimer.c
    The comments mention HRTIMER_ABS and HRTIMER_REL, these symbols don't
    exist, the proper names are HRTIMER_MODE_ABS and HRTIMER_MODE_REL.
    
    Signed-off-by: David Daney <david.daney@cavium.com>
    Cc: Jiri Kosina <trivial@kernel.org>
    Link: http://lkml.kernel.org/r/1363202438-21234-1-git-send-email-ddaney.cavm@gmail.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/hrtimer.c
+++ kernel/hrtimer.c
@@ -1059,7 +1059,8 @@ int __hrtimer_start_range_ns(struct hrtimer *timer, ktime_t tim,
  * @timer:	the timer to be added
  * @tim:	expiry time
  * @delta_ns:	"slack" range for the timer
- * @mode:	expiry mode: absolute (HRTIMER_ABS) or relative (HRTIMER_REL)
+ * @mode:	expiry mode: absolute (HRTIMER_MODE_ABS) or
+ *		relative (HRTIMER_MODE_REL)
  *
  * Returns:
  *  0 on success
@@ -1076,7 +1077,8 @@ EXPORT_SYMBOL_GPL(hrtimer_start_range_ns);
  * hrtimer_start - (re)start an hrtimer on the current CPU
  * @timer:	the timer to be added
  * @tim:	expiry time
- * @mode:	expiry mode: absolute (HRTIMER_ABS) or relative (HRTIMER_REL)
+ * @mode:	expiry mode: absolute (HRTIMER_MODE_ABS) or
+ *		relative (HRTIMER_MODE_REL)
  *
  * Returns:
  *  0 on success
