2fa0167e4156be27e534305403824e59c1f097d1
Joonsoo Kim <iamjoonsoo.kim@lge.com>
2013-04-05 03:16:51 +0100

    ARM: 7694/1: ARM, TCM: initialize TCM in paging_init(), instead of setup_arch()
    tcm_init() call iotable_init() and it use early_alloc variants which
    do memblock allocation. Directly using memblock allocation after
    initializing bootmem should not permitted, because bootmem can't know
    where are additinally reserved.
    So move tcm_init() to a safe place before initalizing bootmem.
    
    (On the U300)
    
    Tested-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -60,7 +60,6 @@
 #include "compat.h"
 #endif
 #include "atags.h"
-#include "tcm.h"
 
 #ifndef MEM_SIZE
 #define MEM_SIZE	(16*1024*1024)
@@ -986,8 +985,6 @@ void __init setup_arch(char **cmdline_p)
 	}
 #endif
 	reserve_crashkernel();
-
-	tcm_init();
 
 #ifdef CONFIG_MULTI_IRQ_HANDLER
 	handle_arch_irq = mdesc->handle_irq;
--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -36,6 +36,7 @@
 #include <asm/user_accessible_timer.h>
 
 #include "mm.h"
+#include "tcm.h"
 
 /*
  * empty_zero_page is a special page that is used for
@@ -1626,6 +1627,7 @@ void __init paging_init(struct machine_desc *mdesc)
 	remap_pages();
 	devicemaps_init(mdesc);
 	kmap_init();
+	tcm_init();
 
 	top_pmd = pmd_off_k(0xffff0000);
 
