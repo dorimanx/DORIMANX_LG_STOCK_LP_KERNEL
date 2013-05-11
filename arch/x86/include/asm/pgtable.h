7d7caa3e22856490aeff203ea848cb67366641b4
Al Viro <viro@zeniv.linux.org.uk>
2013-05-11 12:13:10 -0400

    consolidate io_remap_pfn_range definitions
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Ian Maund <imaund@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/x86/include/asm/pgtable.h
+++ arch/x86/include/asm/pgtable.h
@@ -505,9 +505,6 @@ static inline unsigned long pages_to_mb(unsigned long npg)
 	return npg >> (20 - PAGE_SHIFT);
 }
 
-#define io_remap_pfn_range(vma, vaddr, pfn, size, prot)	\
-	remap_pfn_range(vma, vaddr, pfn, size, prot)
-
 #if PAGETABLE_LEVELS > 2
 static inline int pud_none(pud_t pud)
 {
