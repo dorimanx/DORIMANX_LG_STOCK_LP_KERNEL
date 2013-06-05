2eb669e49bd96eaed9e1c8d33b9930eaa508c729
Al Viro <viro@zeniv.linux.org.uk>
2013-06-05 14:07:08 -0400

    snd_pcm_link(): fix a leak...
    in case when snd_pcm_stream_linked(substream) is true, we end up leaking
    group.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- sound/core/pcm_native.c
+++ sound/core/pcm_native.c
@@ -1681,6 +1681,7 @@ static int snd_pcm_link(struct snd_pcm_substream *substream, int fd)
 	}
 	if (!snd_pcm_stream_linked(substream)) {
 		substream->group = group;
+		group = NULL;
 		spin_lock_init(&substream->group->lock);
 		INIT_LIST_HEAD(&substream->group->substreams);
 		list_add_tail(&substream->link_list, &substream->group->substreams);
@@ -1695,8 +1696,7 @@ static int snd_pcm_link(struct snd_pcm_substream *substream, int fd)
  _nolock:
 	snd_card_unref(substream1->pcm->card);
 	fput_light(file, fput_needed);
-	if (res < 0)
-		kfree(group);
+	kfree(group);
 	return res;
 }
 
