8d50f6cf56b2e6240da08a7af0d319d7f77f5340
Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
2013-06-19 10:40:48 +0100

    ARM: 7763/1: kernel: fix __cpu_logical_map default initialization
    The __cpu_logical_map array is statically initialized to 0, which is a valid
    MPIDR value. To prevent issues with the current implementation, this patch
    defines an MPIDR_INVALID value, and statically initializes the
    __cpu_logical_map[] array to it. Entries in the arm_dt_init_cpu_maps()
    tmp_map array used to stash DT reg properties while parsing DT are initialized
    with the MPIDR_INVALID value as well for consistency.
    
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Acked-by: Nicolas Pitre <nico@linaro.org>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/include/asm/smp_plat.h
+++ arch/arm/include/asm/smp_plat.h
@@ -46,7 +46,7 @@ static inline int cache_ops_need_broadcast(void)
 /*
  * Logical CPU mapping.
  */
-extern int __cpu_logical_map[];
+extern u32 __cpu_logical_map[];
 #define cpu_logical_map(cpu)	__cpu_logical_map[cpu]
 
 #endif
