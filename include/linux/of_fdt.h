1ecdb4dbac9ac105a60e46fd048ab19095200a62
Rob Herring <rob.herring@calxeda.com>
2013-08-26 09:47:40 -0500

    of: Introduce common early_init_dt_scan
    Most architectures scan the all the same items early in the FDT and none
    are really architecture specific. Create a common early_init_dt_scan to
    unify the early scan of root, memory, and chosen nodes in the flattened
    DT.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Acked-by: Grant Likely <grant.likely@linaro.org>
    Signed-off-by: Ian Maund <imaund@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/of_fdt.h
+++ include/linux/of_fdt.h
@@ -114,6 +114,8 @@ extern void early_init_dt_setup_initrd_arch(unsigned long start,
 extern int early_init_dt_scan_root(unsigned long node, const char *uname,
 				   int depth, void *data);
 
+extern bool early_init_dt_scan(void *params);
+
 /* Other Prototypes */
 extern void unflatten_device_tree(void);
 extern void early_init_devtree(void *);
