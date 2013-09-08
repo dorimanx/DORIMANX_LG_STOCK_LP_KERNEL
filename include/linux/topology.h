1cfba1d39940ce58ad1e0dd4be5e352c06e9d8ce
Paul Reioux <reioux@gmail.com>
2013-09-08 17:05:40 -0500

    sched: Consider max cost of idle balance per sched domain
    Date    Thu, 29 Aug 2013 13:05:35 -0700
    
    In this patch, we keep track of the max cost we spend doing idle load balancing
    for each sched domain. If the avg time the CPU remains idle is less then the
    time we have already spent on idle balancing + the max cost of idle balancing
    in the sched domain, then we don't continue to attempt the balance. We also
    keep a per rq variable, max_idle_balance_cost, which keeps track of the max
    time spent on newidle load balances throughout all its domains. Additionally,
    we swap sched_migration_cost used in idle_balance for rq->max_idle_balance_cost.
    
    By using the max, we avoid overrunning the average. This further reduces the chance
    we attempt balancing when the CPU is not idle for longer than the cost to balance.
    
    I also limited the max cost of each domain to 5*sysctl_sched_migration_cost as
    a way to prevent the max from becoming too inflated.
    
    Signed-off-by: Jason Low <jason.low2@hp.com>
    
    backported for Linux 3.4
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- include/linux/topology.h
+++ include/linux/topology.h
@@ -108,6 +108,7 @@ int arch_update_cpu_topology(void);
 	.last_balance		= jiffies,				\
 	.balance_interval	= 1,					\
 	.smt_gain		= 1178,	/* 15% */			\
+	.max_newidle_lb_cost	= 0,					\
 }
 #endif
 #endif /* CONFIG_SCHED_SMT */
@@ -140,6 +141,7 @@ int arch_update_cpu_topology(void);
 				,					\
 	.last_balance		= jiffies,				\
 	.balance_interval	= 1,					\
+	.max_newidle_lb_cost	= 0,					\
 }
 #endif
 #endif /* CONFIG_SCHED_MC */
@@ -173,6 +175,7 @@ int arch_update_cpu_topology(void);
 				,					\
 	.last_balance		= jiffies,				\
 	.balance_interval	= 1,					\
+	.max_newidle_lb_cost	= 0,					\
 }
 #endif
 
--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1558,7 +1558,7 @@ ttwu_do_wakeup(struct rq *rq, struct task_struct *p, int wake_flags)
 
 	if (rq->idle_stamp) {
 		u64 delta = rq->clock - rq->idle_stamp;
-		u64 max = 2*sysctl_sched_migration_cost;
+		u64 max = 2*rq->max_idle_balance_cost;
 
 		update_avg(&rq->avg_idle, delta);
 
@@ -7616,6 +7616,7 @@ void __init sched_init(void)
 		rq->online = 0;
 		rq->idle_stamp = 0;
 		rq->avg_idle = 2*sysctl_sched_migration_cost;
+		rq->max_idle_balance_cost = sysctl_sched_migration_cost;
 		rq->cur_freq = 0;
 		rq->max_freq = 0;
 		rq->min_freq = 0;
