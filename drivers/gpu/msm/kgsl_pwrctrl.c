5d9c2389cce9f04e7ac56378e3f3bfe0da1a12fa
Vladimir Razgulin <vrazguli@codeaurora.org>
2013-11-07 11:26:39 -0700

    msm: kgsl: Wake gpu up on the max freq when using the performance governor
    Use the flag provided by the performance governor on the device
    suspend/slumber to wake it up later on the max gpu frequency.
    
    Change-Id: Ia8284458ffe9cd17eb8e87b2eb84c485ae6e8afa
    Signed-off-by: Vladimir Razgulin <vrazguli@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -1173,6 +1173,7 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 	pwr->active_pwrlevel = pdata->init_level;
 	pwr->default_pwrlevel = pdata->init_level;
 	pwr->init_pwrlevel = pdata->init_level;
+	pwr->wakeup_maxpwrlevel = 0;
 	for (i = 0; i < pdata->num_levels; i++) {
 		pwr->pwrlevels[i].gpu_freq =
 		(pdata->pwrlevel[i].gpu_freq > 0) ?
@@ -1637,11 +1638,18 @@ EXPORT_SYMBOL(kgsl_pwrctrl_wake);
 void kgsl_pwrctrl_enable(struct kgsl_device *device)
 {
 	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
+	int level;
 	/* Order pwrrail/clk sequence based upon platform */
 	kgsl_pwrctrl_pwrrail(device, KGSL_PWRFLAGS_ON);
-
-	if (pwr->constraint.type == KGSL_CONSTRAINT_NONE)
-		kgsl_pwrctrl_pwrlevel_change(device, pwr->active_pwrlevel);
+	if (pwr->constraint.type == KGSL_CONSTRAINT_NONE) {
+		level = pwr->active_pwrlevel;
+	} else if (pwr->wakeup_maxpwrlevel) {
+		level = pwr->max_pwrlevel;
+		pwr->wakeup_maxpwrlevel = 0;
+	} else {
+		level = pwr->default_pwrlevel;
+	}
+	kgsl_pwrctrl_pwrlevel_change(device, level);
 
 	kgsl_pwrctrl_clk(device, KGSL_PWRFLAGS_ON, KGSL_STATE_ACTIVE);
 	kgsl_pwrctrl_axi(device, KGSL_PWRFLAGS_ON);
