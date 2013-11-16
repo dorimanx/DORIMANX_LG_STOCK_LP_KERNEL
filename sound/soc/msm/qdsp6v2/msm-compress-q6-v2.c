b288d69bc05d0395d83ed8349ae61ee5b4f8ac89
Dhananjay Kumar <dhakumar@codeaurora.org>
2013-11-17 00:51:45 +0530

    ASoC: msm: Add MP2 decode support in compress driver
    Add MP2 decoder support to ALSA compressed driver. It
    allows user-space application to play MP2 audio data through
    QDSP6.
    
    Signed-off-by: Dhananjay Kumar <dhakumar@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -563,8 +563,9 @@ static void populate_codec_list(struct msm_compr_audio *prtd)
 	prtd->compr_cap.codecs[1] = SND_AUDIOCODEC_AAC;
 	prtd->compr_cap.codecs[2] = SND_AUDIOCODEC_AC3;
 	prtd->compr_cap.codecs[3] = SND_AUDIOCODEC_EAC3;
+	prtd->compr_cap.codecs[4] = SND_AUDIOCODEC_MP2;
 #ifdef CONFIG_HIFI_SOUND
-	prtd->compr_cap.codecs[4] = SND_AUDIOCODEC_PCM;
+	prtd->compr_cap.codecs[5] = SND_AUDIOCODEC_PCM;
 #endif
 }
 
@@ -627,6 +628,9 @@ static int msm_compr_send_media_format_block(struct snd_compr_stream *cstream,
 	case FORMAT_AC3:
 		break;
 	case FORMAT_EAC3:
+		break;
+	case FORMAT_MP2:
+		pr_debug("%s: SND_AUDIOCODEC_MP2\n", __func__);
 		break;
 	default:
 		pr_debug("%s, unsupported format, skip", __func__);
@@ -1083,6 +1087,13 @@ static int msm_compr_set_params(struct snd_compr_stream *cstream,
 		frame_sz = EAC3_OUTPUT_FRAME_SZ;
 		break;
 	}
+
+	case SND_AUDIOCODEC_MP2: {
+		pr_debug("SND_AUDIOCODEC_MP2\n");
+		prtd->codec = FORMAT_MP2;
+		break;
+	}
+
 	default:
 		pr_err("codec not supported, id =%d\n", params->codec.id);
 		return -EINVAL;
