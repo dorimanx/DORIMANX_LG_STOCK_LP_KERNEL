a782f3f9ef102b59ea262d05983372db4e874eda
Shubhraprakash Das <sadas@codeaurora.org>
2013-11-26 16:44:25 -0800

    msm: kgsl: Use GPU to switch pagetables when current one is default
    Use the GPU to switch pagetables even if the current pagetable is
    the default pagetable. This requires that commands are submitted to
    GPU that do not belong to any context since the current context is
    a NULL one. This is better for performance because if GPU is not
    used to switch pagetable then synchronization is required between the
    GPU and CPU.
    
    Change-Id: I6c1a161fed0747714a86419cdd873ac080466bbf
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1171,11 +1171,8 @@ static int adreno_iommu_setstate(struct kgsl_device *device,
 	num_iommu_units = kgsl_mmu_get_num_iommu_units(&device->mmu);
 
 	context = kgsl_context_get(device, context_id);
-	if (!context) {
-		kgsl_mmu_device_setstate(&device->mmu, flags);
-		return 0;
-	}
-	adreno_ctx = ADRENO_CONTEXT(context);
+	if (context)
+		adreno_ctx = ADRENO_CONTEXT(context);
 
 	link = kmalloc(PAGE_SIZE, GFP_KERNEL);
 	if (link == NULL) {
