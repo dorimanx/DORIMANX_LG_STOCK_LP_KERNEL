9ea45d142d2e6707b1af3eea2993fbcd5d662b07
Colin Cross <ccross@android.com>
2013-12-19 18:37:49 -0800

    gpu: ion: fix overflow and list bugs in system heap
    Fix a few bugs in ion_system_heap:
    
    Initialize the list node in the info block.
    
    Don't store size_remaining in a signed long, allocating >2GB
    could overflow, resulting in a call to sg_alloc_table with
    nents=0 which panics.  alloc_largest_available will never
    return a block larger than size_remanining, so it can never
    go negative.
    
    Limit a single allocation to half of all memory.  Prevents a
    large allocation from taking down the whole system.
    
    Signed-off-by: Colin Cross <ccross@android.com>
    Git-commit: c69de844c80884167bb42243b36498dde81eebf6
    Git-repo: http://android.googlesource.com/kernel/common/
    [mitchelh@codeaurora.org: we already had the signed long fix]
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/gpu/ion/ion_system_heap.c
+++ drivers/gpu/ion/ion_system_heap.c
@@ -140,6 +140,7 @@ static struct page_info *alloc_largest_available(struct ion_system_heap *heap,
 		info->page = page;
 		info->order = orders[i];
 		info->from_pool = from_pool;
+		INIT_LIST_HEAD(&info->list);
 		return info;
 	}
 	kfree(info);
@@ -197,6 +198,9 @@ static int ion_system_heap_allocate(struct ion_heap *heap,
 	struct pages_mem data;
 	unsigned int sz;
 	bool split_pages = ion_buffer_fault_user_mappings(buffer);
+
+	if (size / PAGE_SIZE > totalram_pages / 2)
+		return -ENOMEM;
 
 	data.size = 0;
 	INIT_LIST_HEAD(&pages);
