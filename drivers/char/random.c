56f335ee01ee98268e0cae4fdc02b78e54207df5
dorimanx <yuri@bynet.co.il>
2014-02-03 23:17:37 +0200

    random: fix debug format strings MIME-Version: 1.0 Content-Type: text/plain; charset=UTF-8 Content-Transfer-Encoding: 8bit
    Fix the following warnings in formatting debug output:
    
    drivers/char/random.c: In function âer_secondary_poolâdrivers/char/random.c:827: warning: format ââxpects type âtâbut argument 7 has type âze_târivers/char/random.c: In function âcountâdrivers/char/random.c:859: warning: format ââxpects type âtâbut argument 5 has type âze_târivers/char/random.c:881: warning: format ââxpects type âtâbut argument 5 has type âze_târivers/char/random.c: In function ândom_readâdrivers/char/random.c:1141: warning: format ââxpects type âtâbut argument 5 has type âize_târivers/char/random.c:1145: warning: format ââxpects type âtâbut argument 5 has type âize_târivers/char/random.c:1145: warning: format ââxpects type âtâbut argument 6 has type âng unsigned intâby using '%zd' instead of '%d' to properly denote ssize_t/size_t conversion.
    
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -853,7 +853,7 @@ static void xfer_secondary_pool(struct entropy_store *r, size_t nbytes)
 		bytes = min_t(int, bytes, sizeof(u.tmp));
 
 		DEBUG_ENT("going to reseed %s with %d bits "
-			  "(%d of %d requested)\n",
+			  "(%zu of %d requested)\n",
 			  r->name, bytes * 8, nbytes * 8, r->entropy_count);
 
 		bytes = extract_entropy(r->pull, u.tmp, bytes,
@@ -890,7 +890,7 @@ static size_t account(struct entropy_store *r, size_t nbytes, int min,
 	spin_lock_irqsave(&r->lock, flags);
 
 	BUG_ON(r->entropy_count > r->poolinfo->POOLBITS);
-	DEBUG_ENT("trying to extract %d bits from %s\n",
+	DEBUG_ENT("trying to extract %zu bits from %s\n",
 		  nbytes * 8, r->name);
 
 	/* Can we pull enough? */
@@ -920,7 +920,7 @@ retry:
 		}
 	}
 
-	DEBUG_ENT("debiting %d entropy credits from %s%s\n",
+	DEBUG_ENT("debiting %zu entropy credits from %s%s\n",
 		  nbytes * 8, r->name, r->limit ? "" : " (unlimited)");
 
 	spin_unlock_irqrestore(&r->lock, flags);
@@ -1180,11 +1180,16 @@ random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 		if (n > SEC_XFER_SIZE)
 			n = SEC_XFER_SIZE;
 
-		DEBUG_ENT("reading %d bits\n", n*8);
+		DEBUG_ENT("reading %zu bits\n", n*8);
 
 		n = extract_entropy_user(&blocking_pool, buf, n);
 
-		DEBUG_ENT("read got %d bits (%d still needed)\n",
+		if (n < 0) {
+			retval = n;
+			break;
+		}
+
+		DEBUG_ENT("read got %zd bits (%zd still needed)\n",
 			  n*8, (nbytes-n)*8);
 
 		if (n == 0) {
@@ -1209,10 +1214,6 @@ random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 			continue;
 		}
 
-		if (n < 0) {
-			retval = n;
-			break;
-		}
 		count += n;
 		buf += n;
 		nbytes -= n;
