c6ca0469af4f919ff1171daeea9f247cf0cf7afd
Preeti U Murthy <preeti@linux.vnet.ibm.com>
2014-02-09 11:32:22 +0530

    tick: Fixup more fallout from hrtimer broadcast mode
    The hrtimer mode of broadcast is supported only when
    GENERIC_CLOCKEVENTS_BROADCAST and TICK_ONESHOT config options
    are enabled. Hence compile in the functions for hrtimer mode
    of broadcast only when these options are selected.
    Also fix max_delta_ticks value for the pseudo clock device.
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Reported-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Preeti U Murthy <preeti@linux.vnet.ibm.com>
    Link: http://lkml.kernel.org/r/52F719EE.9010304@linux.vnet.ibm.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/clockchips.h
+++ include/linux/clockchips.h
@@ -196,6 +196,7 @@ static inline int clockevents_notify(unsigned long reason, void *arg) { return 0
 #else /* CONFIG_GENERIC_CLOCKEVENTS_BUILD */
 
 static inline int clockevents_notify(unsigned long reason, void *arg) { return 0; }
+static inline void tick_setup_hrtimer_broadcast(void) {};
 
 #endif
 
