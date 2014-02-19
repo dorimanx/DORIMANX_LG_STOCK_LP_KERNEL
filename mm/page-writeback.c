8d54b67effbdf6c0c9f106a4364ac88e84b3a49f
dorimanx <yuri@bynet.co.il>
2014-02-19 19:27:00 +0200

    Revert "CHROMIUM: mm: Fix calculation of dirtyable memory"
    This reverts commit e2cf9dd031f2db6a1d69d973a10bb444331a1142.

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -196,18 +196,11 @@ static unsigned long highmem_dirtyable_memory(unsigned long total)
 	unsigned long x = 0;
 
 	for_each_node_state(node, N_HIGH_MEMORY) {
-		unsigned long nr_pages;
 		struct zone *z =
 			&NODE_DATA(node)->node_zones[ZONE_HIGHMEM];
 
-		nr_pages = zone_page_state(z, NR_FREE_PAGES) +
-		  zone_reclaimable_pages(z);
-		/*
-		 * make sure that the number of pages for this node
-		 * is never "negative".
-		 */
-		nr_pages -= min(nr_pages, z->dirty_balance_reserve);
-		x += nr_pages;
+		x += zone_page_state(z, NR_FREE_PAGES) +
+		     zone_reclaimable_pages(z) - z->dirty_balance_reserve;
 	}
 	/*
 	 * Unreclaimable memory (kernel memory or anonymous memory
