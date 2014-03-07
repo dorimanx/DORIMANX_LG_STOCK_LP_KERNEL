7962fe221b7640910ae099b106b9abc1013e286b
Tejun Heo <tj@kernel.org>
2014-03-07 10:24:48 -0500

    usb: don't use PREPARE_DELAYED_WORK
    PREPARE_[DELAYED_]WORK() are being phased out.  They have few users
    and a nasty surprise in terms of reentrancy guarantee as workqueue
    considers work items to be different if they don't have the same work
    function.
    
    usb_hub->init_work is multiplexed with multiple work functions;
    however, the work item is never queued while in-flight, so we can
    simply use INIT_DELAYED_WORK() before each queueing.
    
    It would probably be best to route this with other related updates
    through the workqueue tree.
    
    Lightly tested.
    
    v2: Greg and Alan confirm that the work item is never queued while
        in-flight.  Simply use INIT_DELAYED_WORK().
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Alan Stern <stern@rowland.harvard.edu>
    Cc: linux-usb@vger.kernel.org
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -870,7 +870,7 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 			if (hdev->bus->is_b_host)
 				goto init2;
 #endif
-			PREPARE_DELAYED_WORK(&hub->init_work, hub_init_func2);
+			INIT_DELAYED_WORK(&hub->init_work, hub_init_func2);
 			schedule_delayed_work(&hub->init_work,
 					msecs_to_jiffies(delay));
 
@@ -1025,7 +1025,7 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 
 		/* Don't do a long sleep inside a workqueue routine */
 		if (type == HUB_INIT2) {
-			PREPARE_DELAYED_WORK(&hub->init_work, hub_init_func3);
+			INIT_DELAYED_WORK(&hub->init_work, hub_init_func3);
 			schedule_delayed_work(&hub->init_work,
 					msecs_to_jiffies(delay));
 			return;		/* Continues at init3: below */
