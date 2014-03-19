de9433c3e5504dab040f8a2529f869a6650eb72b
dorimanx <yuri@bynet.co.il>
2014-03-19 12:40:19 +0200

    random: entropy tweaks are all the rage nowadays

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -746,6 +746,8 @@ out:
 void add_input_randomness(unsigned int type, unsigned int code,
 				 unsigned int value)
 {
+/* random: prevent add_input from doing anything */
+#if 0
 	static unsigned char last_value;
 
 	/* ignore autorepeat and the like */
@@ -756,6 +758,8 @@ void add_input_randomness(unsigned int type, unsigned int code,
 	last_value = value;
 	add_timer_randomness(&input_timer_state,
 			     (type << 4) ^ code ^ (code >> 4) ^ value);
+#endif
+	return;
 }
 EXPORT_SYMBOL_GPL(add_input_randomness);
 
@@ -1183,6 +1187,12 @@ void rand_initialize_disk(struct gendisk *disk)
 static ssize_t
 random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
+/*
+random: entropy tweaks are all the rage nowadays
+use nonblocking for all.  Read this web page:
+http://lwn.net/Articles/489734/ - WIP
+*/
+#if 0
 	ssize_t n, retval = 0, count = 0;
 
 	if (nbytes == 0)
@@ -1235,6 +1245,8 @@ random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 	}
 
 	return (count ? count : retval);
+#endif
+	return extract_entropy_user(&nonblocking_pool, buf, nbytes);
 }
 
 static ssize_t
