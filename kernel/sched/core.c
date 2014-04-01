dadb40dbbd2ad3718516533d3edea17bd288fac3
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-03-31 17:32:30 -0700

    sched: Track number of big and small tasks on a cpu
    This patch adds 'nr_big_tasks' and 'nr_small_tasks' per-cpu counters
    that tracks number of big and small tasks on a cpu respectively. This
    will be used in load balance decisions introduced in a subsequent
    patch.
    
    Change-Id: Ia174904140f81dd6d1946286889a50be3f16ea83
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1377,6 +1377,8 @@ update_history(struct rq *rq, struct task_struct *p, u32 runtime, int samples)
 	if (p->on_rq) {
 		rq->cumulative_runnable_avg -= p->ravg.demand;
 		BUG_ON((s64)rq->cumulative_runnable_avg < 0);
+		if (p->sched_class == &fair_sched_class)
+			dec_nr_big_small_task(rq, p);
 	}
 
 	avg = div64_u64(sum, RAVG_HIST_SIZE);
@@ -1390,8 +1392,11 @@ update_history(struct rq *rq, struct task_struct *p, u32 runtime, int samples)
 
 	p->ravg.demand = demand;
 
-	if (p->on_rq)
+	if (p->on_rq) {
 		rq->cumulative_runnable_avg += p->ravg.demand;
+		if (p->sched_class == &fair_sched_class)
+			inc_nr_big_small_task(rq, p);
+	}
 }
 
 static int __init set_sched_ravg_window(char *str)
@@ -7616,6 +7621,12 @@ static int cpufreq_notifier_policy(struct notifier_block *nb,
 	load_scale *= load_scale_cpu_freq(cpu);
 	load_scale >>= 10;
 
+	/*
+	 * Changed load_scale_factor can trigger reclassification of tasks as
+	 * big or small. Make this change "atomic" so that tasks are accounted
+	 * properly due to changed load_scale_factor
+	 */
+	pre_big_small_task_count_change();
 	for_each_cpu(i, policy->related_cpus) {
 		struct rq *rq = cpu_rq(i);
 
@@ -7624,6 +7635,7 @@ static int cpufreq_notifier_policy(struct notifier_block *nb,
 	}
 
 	update_min_max_capacity();
+	post_big_small_task_count_change();
 
 	return 0;
 }
@@ -7835,6 +7847,9 @@ void __init sched_init(void)
 		rq->efficiency = 1024;
 		rq->capacity = 1024;
 		rq->load_scale_factor = 1024;
+#endif
+#ifdef CONFIG_SCHED_HMP
+		rq->nr_small_tasks = rq->nr_big_tasks = 0;
 #endif
 
 		INIT_LIST_HEAD(&rq->cfs_tasks);
