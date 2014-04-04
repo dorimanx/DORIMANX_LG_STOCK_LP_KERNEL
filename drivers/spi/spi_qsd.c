1caf78a35ad4bfb7902c12552779abcf894c5c86
Dan Sneddon <dsneddon@codeaurora.org>
2014-04-04 14:50:18 -0600

    spi: spi_qsd: Prevent null pointer access
    Prevent null pointer accesses in msm spi driver.
    
    Signed-off-by: Dan Sneddon <dsneddon@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/spi/spi_qsd.c
+++ drivers/spi/spi_qsd.c
@@ -2556,7 +2556,6 @@ static int msm_spi_probe(struct platform_device *pdev)
 	for (i = 0; i < ARRAY_SIZE(spi_cs_rsrcs); ++i)
 		dd->cs_gpios[i].valid = 0;
 
-	master->rt = pdata->rt_priority;
 	dd->pdata = pdata;
 	resource = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if (!resource) {
@@ -2567,7 +2566,7 @@ static int msm_spi_probe(struct platform_device *pdev)
 	dd->mem_phys_addr = resource->start;
 	dd->mem_size = resource_size(resource);
 
-	if (dd->pdata->use_pinctrl) {
+	if (dd->pdata && dd->pdata->use_pinctrl) {
 		dd->dev = &pdev->dev;
 		rc = msm_spi_pinctrl_init(dd);
 		if (rc)
@@ -2575,6 +2574,7 @@ static int msm_spi_probe(struct platform_device *pdev)
 	}
 
 	if (pdata) {
+		master->rt = pdata->rt_priority;
 		if (pdata->dma_config) {
 			rc = pdata->dma_config();
 			if (rc) {
