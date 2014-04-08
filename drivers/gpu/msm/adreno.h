c4125c4e658949cfb48471d1f333ffba745c6a6f
Jordan Crouse <jcrouse@codeaurora.org>
2014-04-08 12:27:07 -0600

    msm: kgsl: Split out the context and command batch flags
    Split out the context and command batch flags into separate piles.
    In reality it is more like a venn diagram with some context
    exlusive bits some cmdbatch exclusive bits and some shared bits.
    
    The object of all this is to increase the available pool of exclusive
    command batch flags and keep from using up all of the context bits
    too. We still have to be careful for future shared bits so not all of
    the unused field is up for grabs for the command batch but we
    certainly have increased our options.
    
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -440,6 +440,13 @@ struct log_field {
 	{ BIT(KGSL_FT_SKIPCMD), "skipcmd" }
 
 extern struct adreno_gpudev adreno_a2xx_gpudev;
+
+#define ADRENO_CMDBATCH_FLAGS \
+	{ KGSL_CMDBATCH_CTX_SWITCH, "CTX_SWITCH" }, \
+	{ KGSL_CMDBATCH_SYNC, "SYNC" }, \
+	{ KGSL_CMDBATCH_END_OF_FRAME, "EOF" }, \
+	{ KGSL_CMDBATCH_PWR_CONSTRAINT, "PWR_CONSTRAINT" }
+
 extern struct adreno_gpudev adreno_a3xx_gpudev;
 
 /* A2XX register sets defined in adreno_a2xx.c */
