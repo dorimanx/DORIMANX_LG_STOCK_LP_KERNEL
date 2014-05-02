715760cdfb446e4ba4e9e76dcb909a99971c56e8
Ramesh Gupta Guntha <rggupt@codeaurora.org>
2014-05-02 19:39:50 +0530

    qcom: watchdog-v2: Update last_pet during the suspend and resume
    Currently last_pet time is not getting modified during the device
    suspend and resume. Hence update last_pet variable accordingly.
    
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>
    Signed-off-by: Ramesh Gupta Guntha <rggupt@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/mach-msm/msm_watchdog_v2.c
+++ arch/arm/mach-msm/msm_watchdog_v2.c
@@ -122,6 +122,7 @@ static int msm_watchdog_suspend(struct device *dev)
 	__raw_writel(1, wdog_dd->base + WDT0_RST);
 	__raw_writel(0, wdog_dd->base + WDT0_EN);
 	mb();
+	wdog_dd->last_pet = sched_clock();
 	return 0;
 }
 
@@ -134,6 +135,7 @@ static int msm_watchdog_resume(struct device *dev)
 	__raw_writel(1, wdog_dd->base + WDT0_EN);
 	__raw_writel(1, wdog_dd->base + WDT0_RST);
 	mb();
+	wdog_dd->last_pet = sched_clock();
 	return 0;
 }
 
