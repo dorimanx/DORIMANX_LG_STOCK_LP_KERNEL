cb4ceca04391f0ce9d03c2524bf3694f6fe874a3
myfluxi <linflux@arcor.de>
2014-05-14 18:04:21 +0200

    msm: rq_stats: Calculate load based on current freq limit
    We are not using rq_stats any more so this commit is just for the
    sake of completeness.  Prior to this patch, load has been calculated
    based on an initial value of policy.cpuinfo.max_freq. This value may
    change for several reasons: userspace settings, thermal throttling
    (hello hammerhead!) or extended frequency tables where the absolute
    maximum level is often not used during regular operations. This
    results in unreliable calculations. Consider current policy.max to
    fix this behaviour.

--- arch/arm/mach-msm/msm_rq_stats.c
+++ arch/arm/mach-msm/msm_rq_stats.c
@@ -67,11 +67,16 @@ static inline u64 get_cpu_iowait_time(unsigned int cpu, u64 *wall)
 
 static int update_average_load(unsigned int freq, unsigned int cpu)
 {
-
-	struct cpu_load_data *pcpu = &per_cpu(cpuload, cpu);
+	int ret;
 	u64 cur_wall_time, cur_idle_time, cur_iowait_time;
 	unsigned int idle_time, wall_time, iowait_time;
 	unsigned int cur_load, load_at_max_freq;
+	struct cpu_load_data *pcpu = &per_cpu(cpuload, cpu);
+	struct cpufreq_policy policy;
+
+        ret = cpufreq_get_policy(&policy, cpu);
+        if (ret)
+                return -EINVAL;
 
 	cur_idle_time = get_cpu_idle_time(cpu, &cur_wall_time, 0);
 	cur_iowait_time = get_cpu_iowait_time(cpu, &cur_wall_time);
@@ -94,7 +99,7 @@ static int update_average_load(unsigned int freq, unsigned int cpu)
 	cur_load = 100 * (wall_time - idle_time) / wall_time;
 
 	/* Calculate the scaled load across CPU */
-	load_at_max_freq = (cur_load * freq) / pcpu->policy_max;
+	load_at_max_freq = (cur_load * policy.cur) / policy.max;
 
 	if (!pcpu->avg_load_maxfreq) {
 		/* This is the first sample in this window*/
