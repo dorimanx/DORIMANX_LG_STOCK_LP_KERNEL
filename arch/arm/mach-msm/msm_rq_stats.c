5cdea305799a3c3b130ee98638a84531f7b4aad7
Anji Jonnala <anjir@codeaurora.org>
2014-05-20 20:02:34 +0530

    soc: qcom: rq_stats: remove the redundant iowait check
    Iowait_time check is redundant and it would lead to heavy load
    due to miscaluculation of idle time. Hence remove this check
    as iowait will considered based on io_busy flag.
    
    [myfluxi: Adapt to msm 8974]
    
    Change-Id: I4dd9f9c79205eca588daea8cc0fe892394a83391
    CRs-fixed: 662052
    Signed-off-by: Anji Jonnala <anjir@codeaurora.org>

--- arch/arm/mach-msm/msm_rq_stats.c
+++ arch/arm/mach-msm/msm_rq_stats.c
@@ -43,7 +43,6 @@ struct notifier_block cpu_hotplug;
 struct cpu_load_data {
 	u64 prev_cpu_idle;
 	u64 prev_cpu_wall;
-	u64 prev_cpu_iowait;
 	unsigned int avg_load_maxfreq;
 	unsigned int samples;
 	unsigned int window_size;
@@ -57,21 +56,11 @@ static unsigned int lock_hotplug_disabled = 1;
 
 static DEFINE_PER_CPU(struct cpu_load_data, cpuload);
 
-static inline u64 get_cpu_iowait_time(unsigned int cpu, u64 *wall)
-{
-	u64 iowait_time = get_cpu_iowait_time_us(cpu, wall);
-
-	if (iowait_time == -1ULL)
-		return 0;
-
-	return iowait_time;
-}
-
 static int update_average_load(unsigned int freq, unsigned int cpu)
 {
 	int ret;
-	u64 cur_wall_time, cur_idle_time, cur_iowait_time;
-	unsigned int idle_time, wall_time, iowait_time;
+	u64 cur_wall_time, cur_idle_time;
+	unsigned int idle_time, wall_time;
 	unsigned int cur_load, load_at_max_freq;
 	struct cpu_load_data *pcpu = &per_cpu(cpuload, cpu);
 	struct cpufreq_policy policy;
@@ -88,12 +77,6 @@ static int update_average_load(unsigned int freq, unsigned int cpu)
 
 	idle_time = (unsigned int) (cur_idle_time - pcpu->prev_cpu_idle);
 	pcpu->prev_cpu_idle = cur_idle_time;
-
-	iowait_time = (unsigned int) (cur_iowait_time - pcpu->prev_cpu_iowait);
-	pcpu->prev_cpu_iowait = cur_iowait_time;
-
-	if (idle_time >= iowait_time)
-		idle_time -= iowait_time;
 
 	if (unlikely(!wall_time || wall_time < idle_time))
 		return 0;
