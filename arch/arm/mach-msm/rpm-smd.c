bd56bfc72293887c81d542b2aa30932228b383a0
Pranav Vashi <neobuddy89@gmail.com>
2014-05-20 14:28:14 +0530

    msm: Ammend use of interruptible threads
    In commits 9bdb55867a042611be0ec4efb47d8202bfeaae54 and adf59281d2c3fa68714e4acb8ef7078e1dd77c97,
    we changed wait events to use interruptible threads
    to reduce high load avg.
    
    This commit just ammends the use of interruptible threads
    to serve the actual purpose of original wait events.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/mach-msm/rpm-smd.c
+++ arch/arm/mach-msm/rpm-smd.c
@@ -850,7 +850,8 @@ static void msm_rpm_smd_work(struct work_struct *work)
 	char buf[MAX_ERR_BUFFER_SIZE] = {0};
 
 	while (1) {
-		wait_for_completion_interruptible(&data_ready);
+		while (wait_for_completion_interruptible(
+			&data_ready) != 0);
 
 		spin_lock(&msm_rpm_data.smd_lock_read);
 		while (smd_is_pkt_avail(msm_rpm_data.ch_info)) {
--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -2292,14 +2292,10 @@ static int __mdss_fb_display_thread(void *data)
 				mfd->index);
 
 	while (1) {
-		ret = wait_event_interruptible(mfd->commit_wait_q,
-				(atomic_read(&mfd->commits_pending) ||
-				 kthread_should_stop()));
-
-		if (ret) {
-			pr_info("%s: interrupted", __func__);
-			continue;
-		}
+		while (wait_event_interruptible(
+			mfd->commit_wait_q,
+			(atomic_read(&mfd->commits_pending) ||
+			kthread_should_stop())) != 0);
 
 		if (kthread_should_stop())
 			break;
