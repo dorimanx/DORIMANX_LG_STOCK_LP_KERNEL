a13ca8e68f3d8f1c520d6eb0a5fc35a68f61cbf3
Praveen Chidambaram <pchidamb@codeaurora.org>
2014-05-21 16:21:31 -0600

    QoS: Enhance framework to support cpu/irq specific QoS requests
    QoS request for CPU_DMA_LATENCY can be better optimized if the request
    can be set only for the required cpus and not all cpus. This helps save
    power on other cores, while still gauranteeing the quality of service.
    
    Enhance the QoS constraints data structures to support target value for
    each core. Requests specify if the QoS is applicable to all cores
    (default) or to a selective subset of the cores or to a core(s), that the
    IRQ is affine to.
    
    QoS requests that need to track an IRQ can be set to apply only on the
    cpus to which the IRQ's smp_affinity attribute is set to. The QoS
    framework will automatically track IRQ migration between the cores. The
    QoS is updated to be applied only to the core(s) that the IRQ has been
    migrated to.
    
    Idle and interested drivers can request a PM QoS value for a constraint
    across all cpus, or a specific cpu or a set of cpus. Separate APIs have
    been added to request for individual cpu or a cpumask.  The default
    behaviour of PM QoS is maintained i.e, requests that do not specify a
    type of the request will continue to be effected on all cores.  Requests
    that want to specify an affinity of cpu(s) or an irq, can modify the PM
    QoS request data structures by specifying the type of the request and
    either the mask of the cpus or the IRQ number depending on the type.
    Updating the request does not reset the type of the request.
    
    The userspace sysfs interface does not support CPU/IRQ affinity.
    
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/pm_qos.h
+++ include/linux/pm_qos.h
@@ -14,6 +14,8 @@
 #include <linux/miscdevice.h>
 #include <linux/device.h>
 #include <linux/workqueue.h>
+#include <linux/cpumask.h>
+#include <linux/interrupt.h>
 
 enum {
 	PM_QOS_RESERVED = 0,
@@ -76,7 +78,22 @@ enum pm_qos_flags_status {
 #define PM_QOS_FLAG_NO_POWER_OFF	(1 << 0)
 #define PM_QOS_FLAG_REMOTE_WAKEUP	(1 << 1)
 
+enum pm_qos_req_type {
+	PM_QOS_REQ_ALL_CORES = 0,
+	PM_QOS_REQ_AFFINE_CORES,
+#ifdef CONFIG_SMP
+	PM_QOS_REQ_AFFINE_IRQ,
+#endif
+};
+
 struct pm_qos_request {
+	enum pm_qos_req_type type;
+	struct cpumask cpus_affine;
+#ifdef CONFIG_SMP
+	uint32_t irq;
+	/* Internal structure members */
+	struct irq_affinity_notify irq_notify;
+#endif
 	struct plist_node node;
 	int pm_qos_class;
 	int priority;
@@ -173,6 +190,7 @@ struct pm_qos_bounded_constraint {
 struct pm_qos_constraints {
 	struct plist_head list;
 	s32 target_value;	/* Do not change to 64 bit */
+	s32 target_per_cpu[NR_CPUS];
 	s32 default_value;
 	enum pm_qos_type type;
 	struct blocking_notifier_head *notifiers;
@@ -240,6 +258,8 @@ s32 pm_qos_read_min_bound(int pm_qos_bounded_class);
 s32 pm_qos_read_max_bound(int pm_qos_bounded_class);
 
 int pm_qos_request(int pm_qos_class);
+int pm_qos_request_for_cpu(int pm_qos_class, int cpu);
+int pm_qos_request_for_cpumask(int pm_qos_class, struct cpumask *mask);
 int pm_qos_add_notifier(int pm_qos_class, struct notifier_block *notifier);
 int pm_qos_remove_notifier(int pm_qos_class, struct notifier_block *notifier);
 int pm_qos_request_active(struct pm_qos_request *req);
