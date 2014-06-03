ea61957212352dd1d01d855362e21a7cd21d6f7c
dorimanx <yuri@bynet.co.il>
2014-06-03 05:06:27 +0300

    MSM CPUFREQ: Small fix nothing important.

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -279,10 +279,12 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
 	policy->min = CONFIG_MSM_CPU_FREQ_MIN;
 	policy->max = CONFIG_MSM_CPU_FREQ_MAX;
-#endif
-
+#else
+#ifdef CONFIG_ARCH_MSM8974
 	policy->max = 2265600;
 	policy->min = 300000;
+#endif
+#endif
 
 	if (is_clk)
 		cur_freq = clk_get_rate(cpu_clk[policy->cpu])/1000;
@@ -305,7 +307,7 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	if (ret)
 		return ret;
 	pr_debug("cpufreq: cpu%d init at %d switching to %d\n",
-			policy->cpu, cur_freq, table[index].frequency);
+			policy->cpu, cur_freq, policy->max);
 	policy->cur = policy->max;
 
 	policy->cpuinfo.transition_latency =
