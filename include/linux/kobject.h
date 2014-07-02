abb16ae54752965642e56e01a967803ba614bd9e
Jiangli Yuan <a6808c@motorola.com>
2014-07-03 03:39:12 +0800

    kobject: Fix "add_uevent_var: too many keys" warn.
     Double the UEVENT_NUM_ENVP to 64 to fix the "too many keys" warn.
    
    Signed-off-by: Jiangli Yuan <a6808c@motorola.com>
    Signed-off-by: Jane Zhou <a17711@motorola.com>
    Reviewed-on: http://gerrit.mot.com/651680
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.mot.com/746800
    SME-Granted: SME Approvals Granted
    Reviewed-by: Jiang-Li Yuan <jlyuan@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/kobject.h
+++ include/linux/kobject.h
@@ -30,9 +30,9 @@
 #define UEVENT_HELPER_PATH_LEN		256
 
 #ifdef CONFIG_LGE_PM_BATTERY_ID_CHECKER
-#define UEVENT_NUM_ENVP			33	/* number of env pointers */
+#define UEVENT_NUM_ENVP			64	/* number of env pointers */
 #else
-#define UEVENT_NUM_ENVP			32	/* number of env pointers */
+#define UEVENT_NUM_ENVP			64	/* number of env pointers */
 #endif
 
 #define UEVENT_BUFFER_SIZE		2048	/* buffer for the variables */
