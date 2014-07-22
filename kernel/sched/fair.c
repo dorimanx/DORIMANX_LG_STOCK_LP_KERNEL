8c316f145ba3868772de44ca54a84934dd240f7f
Steve Muckle <smuckle@codeaurora.org>
2014-07-22 15:12:32 -0700

    sched: fixes for compilation without CONFIG_SCHED_HMP
    These fixes are necessary to compile without CONFIG_SCHED_HMP
    enabled.
    
    Change-Id: Iabbde3c22a81288242ed3a44fdfdb2a16db8b072
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1831,11 +1831,6 @@ static inline int power_cost(struct task_struct *p, int cpu)
 	return SCHED_POWER_SCALE;
 }
 
-static unsigned int power_cost_at_freq(int cpu, unsigned int freq)
-{
-	return 1;
-}
-
 static inline int
 spill_threshold_crossed(struct task_struct *p, struct rq *rq, int cpu)
 {
@@ -5843,6 +5838,7 @@ ret:
 	return NULL;
 }
 
+#ifdef CONFIG_SCHED_HMP
 static struct rq *find_busiest_queue_hmp(struct lb_env *env,
 				     struct sched_group *group)
 {
@@ -5864,6 +5860,13 @@ static struct rq *find_busiest_queue_hmp(struct lb_env *env,
 
 	return busiest;
 }
+#else
+static inline struct rq *find_busiest_queue_hmp(struct lb_env *env,
+				     struct sched_group *group)
+{
+	return NULL;
+}
+#endif
 
 /*
  * find_busiest_queue - find the busiest runqueue among the cpus in group.
@@ -6719,6 +6722,7 @@ end:
 	clear_bit(NOHZ_BALANCE_KICK, nohz_flags(this_cpu));
 }
 
+#ifdef CONFIG_SCHED_HMP
 static inline int _nohz_kick_needed_hmp(struct rq *rq, int *type)
 {
 	struct sched_domain *sd;
@@ -6753,6 +6757,12 @@ static inline int _nohz_kick_needed_hmp(struct rq *rq, int *type)
 
 	return 0;
 }
+#else
+static inline int _nohz_kick_needed_hmp(struct rq *rq, int *type)
+{
+	return 0;
+}
+#endif
 
 static inline int _nohz_kick_needed(struct rq *rq, int *type)
 {
--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1479,6 +1479,7 @@ static struct task_struct *pick_highest_pushable_task(struct rq *rq, int cpu)
 
 static DEFINE_PER_CPU(cpumask_var_t, local_cpu_mask);
 
+#ifdef CONFIG_SCHED_HMP
 static int find_lowest_rq_hmp(struct task_struct *task)
 {
 	struct cpumask *lowest_mask = __get_cpu_var(local_cpu_mask);
@@ -1519,6 +1520,12 @@ static int find_lowest_rq_hmp(struct task_struct *task)
 	}
 	return best_cpu;
 }
+#else
+static int find_lowest_rq_hmp(struct task_struct *task)
+{
+	return -1;
+}
+#endif
 
 static int find_lowest_rq(struct task_struct *task)
 {
