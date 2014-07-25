e823e2a690c4fa1c976b28a7c88857ff433075eb
Bhalchandra Gajare <gajare@codeaurora.org>
2014-07-24 17:20:53 -0700

    ASoC: qdsp6v2: Add support for LSM_SET_PARAMS
    Add LSM_SET_PARAMS interface to the listen driver so as to provide
    flexibility to the user space to be able to set the parameters for
    listen session independently of the sound model registration.
    
    Change-Id: Ic483eea4e07875a0e504ce920ff74706867d5e75
    Signed-off-by: Bhalchandra Gajare <gajare@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sound/soc/msm/qdsp6v2/q6lsm.c
+++ sound/soc/msm/qdsp6v2/q6lsm.c
@@ -597,21 +597,31 @@ exit:
 	return rc;
 }
 
-int q6lsm_register_sound_model(struct lsm_client *client,
-			       enum lsm_detection_mode mode,
-			       bool detectfailure)
+int q6lsm_set_data(struct lsm_client *client,
+			   enum lsm_detection_mode mode,
+			   bool detectfailure)
 {
-	int rc;
-	struct lsm_cmd_reg_snd_model cmd;
+	int rc = 0;
 
-	memset(&cmd, 0, sizeof(cmd));
+	if (!client->confidence_levels) {
+		/*
+		 * It is possible that confidence levels are
+		 * not provided. This is not a error condition.
+		 * Return gracefully without any error
+		 */
+		pr_debug("%s: no conf levels to set\n",
+			__func__);
+		return rc;
+	}
+
 	if (mode == LSM_MODE_KEYWORD_ONLY_DETECTION) {
 		client->mode = 0x01;
 	} else if (mode == LSM_MODE_USER_KEYWORD_DETECTION) {
 		client->mode = 0x03;
 	} else {
 		pr_err("%s: Incorrect detection mode %d\n", __func__, mode);
-		return -EINVAL;
+		rc = -EINVAL;
+		goto err_ret;
 	}
 	client->mode |= detectfailure << 2;
 	client->connect_to_port = AFE_PORT_ID_SLIMBUS_MULTI_CHAN_5_TX;
@@ -620,11 +630,30 @@ int q6lsm_register_sound_model(struct lsm_client *client,
 	if (rc < 0) {
 		pr_err("%s: Failed to set lsm config params %d\n",
 			__func__, rc);
-		return rc;
+		goto err_ret;
 	}
 	rc = q6lsm_send_cal(client);
 	if (rc < 0) {
 		pr_err("%s: Failed to send calibration data %d\n",
+			__func__, rc);
+		goto err_ret;
+	}
+
+err_ret:
+	return rc;
+}
+
+int q6lsm_register_sound_model(struct lsm_client *client,
+			       enum lsm_detection_mode mode,
+			       bool detectfailure)
+{
+	int rc;
+	struct lsm_cmd_reg_snd_model cmd;
+
+	memset(&cmd, 0, sizeof(cmd));
+	rc = q6lsm_set_data(client, mode, detectfailure);
+	if (rc) {
+		pr_err("%s: Failed to set lsm data, err = %d\n",
 			__func__, rc);
 		return rc;
 	}
@@ -971,17 +1000,18 @@ int q6lsm_snd_model_buf_alloc(struct lsm_client *client, size_t len)
 			goto fail;
 		}
 		pr_debug("%s: Length = 0x%x\n", __func__, len);
-        client->lsm_cal_phy_addr = (pad_zero +
-                        client->sound_model.phys +
-                        client->sound_model.size);
-        client->lsm_cal_size = lsm_cal.cal_size;
-        memcpy((client->sound_model.data + pad_zero + client->sound_model.size),
-               (uint32_t *)lsm_cal.cal_kvaddr, client->lsm_cal_size);
-        pr_debug("%s: Copy cal start virt_addr %pa phy_addr %pa\n"
-                 "Offset cal virtual Addr %pa\n", __func__,
-                 client->sound_model.data, &client->sound_model.phys,
-                 (pad_zero + client->sound_model.data +
-                 client->sound_model.size));
+        	client->lsm_cal_phy_addr = (pad_zero +
+                        	client->sound_model.phys +
+                        	client->sound_model.size);
+        	client->lsm_cal_size = lsm_cal.cal_size;
+			memcpy((client->sound_model.data + pad_zero +
+			client->sound_model.size),
+               		(uint32_t *)lsm_cal.cal_kvaddr, client->lsm_cal_size);
+        	pr_debug("%s: Copy cal start virt_addr %p phy_addr %pa\n"
+                 	"Offset cal virtual Addr %p\n", __func__,
+                 	client->sound_model.data, &client->sound_model.phys,
+                 	(pad_zero + client->sound_model.data +
+                 	client->sound_model.size));
 	} else {
 		pr_err("%s: sound model busy\n", __func__);
 		rc = -EBUSY;
