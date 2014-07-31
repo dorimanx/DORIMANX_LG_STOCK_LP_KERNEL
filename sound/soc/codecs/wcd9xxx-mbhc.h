a81ff3b12150d1d225ebba33e36ee729a7bbea9b
Sudheer Papothi <spapothi@codeaurora.org>
2014-08-01 05:05:24 +0530

    ASoC: wcd9xxx: Add HPH auto pull down support
    Add support for headphone auto pull down on WCD9330
    to avoid noise while plugging headphone in car stereo.
    
    Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sound/soc/codecs/wcd9xxx-mbhc.h
+++ sound/soc/codecs/wcd9xxx-mbhc.h
@@ -286,6 +286,10 @@ struct wcd9xxx_mbhc_cb {
 	int (*enable_mb_source) (struct snd_soc_codec *, bool, bool);
 	void (*setup_int_rbias) (struct snd_soc_codec *, bool);
 	void (*pull_mb_to_vddio) (struct snd_soc_codec *, bool);
+	bool (*insert_rem_status) (struct snd_soc_codec *);
+	void (*micbias_pulldown_ctrl) (struct wcd9xxx_mbhc *, bool);
+	int (*codec_rco_ctrl) (struct snd_soc_codec *, bool);
+	void (*hph_auto_pulldown_ctrl) (struct snd_soc_codec *, bool);
 	struct firmware_cal * (*get_hwdep_fw_cal) (struct snd_soc_codec *,
 				enum wcd_cal_type);
 
