8427cc569c02ab6b68e425db02f6ab019e935c48
Wang Sheng-Hui <shhuiw@gmail.com>
2014-08-06 16:07:07 -0700

    mm: update the description for vm_total_pages
    vm_total_pages is calculated by nr_free_pagecache_pages(), which counts
    the number of pages which are beyond the high watermark within all
    zones.  So vm_total_pages is not equal to total number of pages which
    the VM controls.
    
    Signed-off-by: Wang Sheng-Hui <shhuiw@gmail.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -143,7 +143,11 @@ struct scan_control {
  * From 0 .. 100.  Higher means more swappy.
  */
 int vm_swappiness = 60;
-unsigned long vm_total_pages;	/* The total number of pages which the VM controls */
+/*
+ * The total number of pages which are beyond the high watermark within all
+ * zones.
+ */
+unsigned long vm_total_pages;
 
 #ifdef CONFIG_KSWAPD_CPU_AFFINITY_MASK
 char *kswapd_cpu_mask = CONFIG_KSWAPD_CPU_AFFINITY_MASK;
