822c55c7bf65ee4b9bf94a40537b9e5e879e8228
Dorimanx <yuri@bynet.co.il>
2014-09-03 17:35:29 +0300

    SOUND CONTROL: derp for me :)

--- sound/soc/codecs/wcd9320.c
+++ sound/soc/codecs/wcd9320.c
@@ -4624,7 +4624,7 @@ static int taiko_volatile(struct snd_soc_codec *ssc, unsigned int reg)
 	if (reg == TAIKO_A_RX_HPH_CNP_EN)
 		return 1;
 
-#ifdef CONFIG_SOUND_CONTROL
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
 	/* HPH gain registers */
 	if (reg == TAIKO_A_RX_HPH_L_GAIN || reg == TAIKO_A_RX_HPH_R_GAIN)
 		return 1;
