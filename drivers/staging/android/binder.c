48bdf81f74ec3248a6cf310421bc82b5afd7db6e
Pranav Vashi <neobuddy89@gmail.com>
2014-09-18 22:55:12 +0530

    staging: binder: Do not debug failed transactions
    * Once a failed transaction is caught, there is no ratelimit
      and therefore, kernel panics with wdog_bark.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -106,7 +106,7 @@ enum {
 	BINDER_DEBUG_TOP_ERRORS		    = 1U << 16,
 };
 static uint32_t binder_debug_mask = BINDER_DEBUG_USER_ERROR |
-	BINDER_DEBUG_FAILED_TRANSACTION | BINDER_DEBUG_DEAD_TRANSACTION;
+	BINDER_DEBUG_DEAD_TRANSACTION;
 module_param_named(debug_mask, binder_debug_mask, uint, S_IWUSR | S_IRUGO);
 
 static bool binder_debug_no_lock;
