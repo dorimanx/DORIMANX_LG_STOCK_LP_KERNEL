942eadbb587b0cb1ed23c3b937542005812c724b
Ruchi Kandoi <kandoiruchi@google.com>
2014-10-29 10:36:27 -0700

    power: Adds functionality to log the last suspend abort reason.
    Extends the last_resume_reason to log suspend abort reason. The abort
    reasons will have "Abort:" appended at the start to distinguish itself
    from the resume reason.
    
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
    Change-Id: I3207f1844e3d87c706dfc298fb10e1c648814c5f

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -19,7 +19,7 @@
 #include <linux/kmod.h>
 #include <linux/wakelock.h>
 #include "power.h"
-
+#include <linux/wakeup_reason.h>
 /* 
  * Timeout for stopping processes
  */
@@ -36,6 +36,7 @@ static int try_to_freeze_tasks(bool user_only)
 	unsigned int elapsed_msecs;
 	bool wakeup = false;
 	int sleep_usecs = USEC_PER_MSEC;
+	char suspend_abort[MAX_SUSPEND_ABORT_LEN];
 
 	do_gettimeofday(&start);
 
@@ -76,6 +77,9 @@ static int try_to_freeze_tasks(bool user_only)
 			break;
 
 		if (pm_wakeup_pending()) {
+			pm_get_active_wakeup_sources(suspend_abort,
+				MAX_SUSPEND_ABORT_LEN);
+			log_suspend_abort_reason(suspend_abort);
 			wakeup = true;
 			break;
 		}
