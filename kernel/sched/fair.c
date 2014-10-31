b22ab82fffb4ca0144b47794fe32968a33445096
Dorimanx <yuri@bynet.co.il>
2014-10-31 17:00:21 +0200

    Revert "Disable gentle_fair_sleepers & arch_power in kernel/sched/fair.c"
    This reverts commit 6af95527d7d9dd3ad18fe7876cee10fb95ce0742.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2444,8 +2444,8 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 }
 
-static unsigned int Lgentle_fair_sleepers = 0;
-static unsigned int Larch_power = 0;
+static unsigned int Lgentle_fair_sleepers = 1;
+static unsigned int Larch_power = 1;
 
 void relay_gfs(unsigned int gfs)
 {
