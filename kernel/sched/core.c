cf8f2aa0f017ecbe5c59420c54a35c837afcb068
Rom Lemarchand <romlem@android.com>
2014-11-07 12:48:17 -0800

    cgroup: refactor allow_attach function into common code
    move cpu_cgroup_allow_attach to a common subsys_cgroup_allow_attach.
    This allows any process with CAP_SYS_NICE to move tasks across cgroups if
    they use this function as their allow_attach handler.
    
    Bug: 18260435
    Change-Id: I6bb4933d07e889d0dc39e33b4e71320c34a2c90f
    Signed-off-by: Rom Lemarchand <romlem@android.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9195,23 +9195,6 @@ static void cpu_cgroup_destroy(struct cgroup *cgrp)
 	sched_destroy_group(tg);
 }
 
-static int
-cpu_cgroup_allow_attach(struct cgroup *cgrp, struct cgroup_taskset *tset)
-{
-	const struct cred *cred = current_cred(), *tcred;
-	struct task_struct *task;
-
-	cgroup_taskset_for_each(task, cgrp, tset) {
-		tcred = __task_cred(task);
-
-		if ((current != task) && !capable(CAP_SYS_NICE) &&
-		    cred->euid != tcred->uid && cred->euid != tcred->suid)
-			return -EACCES;
-	}
-
-	return 0;
-}
-
 static int cpu_cgroup_can_attach(struct cgroup *cgrp,
 				 struct cgroup_taskset *tset)
 {
@@ -9604,7 +9587,7 @@ struct cgroup_subsys cpu_cgroup_subsys = {
 	.destroy	= cpu_cgroup_destroy,
 	.can_attach	= cpu_cgroup_can_attach,
 	.attach		= cpu_cgroup_attach,
-	.allow_attach	= cpu_cgroup_allow_attach,
+	.allow_attach	= subsys_cgroup_allow_attach,
 	.exit		= cpu_cgroup_exit,
 	.subsys_id	= cpu_cgroup_subsys_id,
 	.base_cftypes	= cpu_files,
