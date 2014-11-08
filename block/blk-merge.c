5717f8eb5eff0eaf57f28220d82d53449869b375
Dorimanx <yuri@bynet.co.il>
2014-11-08 18:01:49 +0200

    Revert "security: selinux: Add Per-File-Encryption hooks"
    This reverts commit 3eede98cc0a0c4d37e9734f0c5bca5562c4ea121.

--- block/blk-merge.c
+++ block/blk-merge.c
@@ -6,7 +6,6 @@
 #include <linux/bio.h>
 #include <linux/blkdev.h>
 #include <linux/scatterlist.h>
-#include <linux/security.h>
 
 #include "blk.h"
 
@@ -508,10 +507,6 @@ bool blk_rq_merge_ok(struct request *rq, struct bio *bio)
 
 	/* only merge integrity protected bio into ditto rq */
 	if (bio_integrity(bio) != blk_integrity_rq(rq))
-		return false;
-
-	/* Don't merge bios of files with different encryption */
-	if (!security_allow_merge_bio(rq->bio, bio))
 		return false;
 
 	return true;
