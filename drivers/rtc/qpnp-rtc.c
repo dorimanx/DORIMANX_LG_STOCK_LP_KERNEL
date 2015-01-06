8a80c56f2536d5d30ad2661fbb66142724878b58
Mohit Aggarwal <maggarwa@codeaurora.org>
2015-01-06 13:03:49 +0530

    rtc: alarm: Change wake-up source
    Currently, RTC_ALARM is used to wake-up target from
    suspend state and is also used for power-off alarm
    feature. This patch uses qtimer to wake-up from
    suspend state.
    
    Change-Id: Ia42cfecd573309be2f03c18b4f1c321be8202d7d
    Signed-off-by: Mohit Aggarwal <maggarwa@codeaurora.org>

--- drivers/rtc/qpnp-rtc.c
+++ drivers/rtc/qpnp-rtc.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -49,10 +49,11 @@
 #ifdef CONFIG_LGE_PM_RTC_PWROFF_ALARM
 extern bool poweron_alarm;
 #else
-static bool poweron_alarm;
+bool poweron_alarm;
 #endif
 module_param(poweron_alarm, bool, 0644);
 MODULE_PARM_DESC(poweron_alarm, "Enable/Disable power-on alarm");
+EXPORT_SYMBOL(poweron_alarm);
 
 /* rtc driver internal structure */
 struct qpnp_rtc {
