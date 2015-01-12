042b2160bf04f5045a810993c40d890a90250cf1
Dmitry Shmidt <dimitrysh@google.com>
2015-01-12 13:42:05 -0800

    wlan: Add get_wake_irq functionality
    Change-Id: Ic41f06c509b2e625dc9ec4131903db6920c5fd4e
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- include/linux/wlan_plat.h
+++ include/linux/wlan_plat.h
@@ -22,6 +22,7 @@ struct wifi_platform_data {
 	void *(*mem_prealloc)(int section, unsigned long size);
 	int (*get_mac_addr)(unsigned char *buf);
 	void *(*get_country_code)(char *ccode);
+	int (*get_wake_irq)(void);
 };
 
 #endif
