1c1d078e7cb010bb1df2cd0dfa7472756df4220c
Bhalchandra Gajare <gajare@codeaurora.org>
2015-02-10 14:44:36 -0800

    ASoC: q6lsm: Add check for integer overflow
    During sound model registration, the total memory size needed by the
    sound model data is the sum of sound model length, number of zero
    padding bytes and the calibration size. It is possible this sum
    can result into integer overflow causing difficult to debug issues.
    Add check for integer overflow to avoid such possible issues.
    
    Signed-off-by: Bhalchandra Gajare <gajare@codeaurora.org>
    Signed-off-by: Deven Patel <cdevenp@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sound/soc/msm/qdsp6v2/q6lsm.c
+++ sound/soc/msm/qdsp6v2/q6lsm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014, Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2015, Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -943,8 +943,19 @@ int q6lsm_snd_model_buf_alloc(struct lsm_client *client, size_t len)
 	if (!client->sound_model.data) {
 		client->sound_model.size = len;
 		pad_zero = (LSM_ALIGN_BOUNDARY -
-			   (len % LSM_ALIGN_BOUNDARY));
-		total_mem = PAGE_ALIGN(pad_zero + len + lsm_cal.cal_size);
+			    (len % LSM_ALIGN_BOUNDARY));
+		if ((len > SIZE_MAX - pad_zero) ||
+		    (len + pad_zero >
+		     SIZE_MAX - lsm_cal.cal_size)) {
+			pr_err("%s: invalid allocation size, len = %zd, pad_zero =%zd, cal_size = %zd\n",
+				__func__, len, pad_zero,
+				lsm_cal.cal_size);
+			rc = -EINVAL;
+			goto fail;
+		}
+
+		total_mem = PAGE_ALIGN(pad_zero + len +
+			lsm_cal.cal_size);
 		pr_debug("%s: Pad zeros sound model %zd Total mem %zd\n",
 				 __func__, pad_zero, total_mem);
 		rc = msm_audio_ion_alloc("lsm_client",
