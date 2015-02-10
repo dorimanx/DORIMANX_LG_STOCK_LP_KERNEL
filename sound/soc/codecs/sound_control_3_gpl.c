35562a54da0764a43fb41c437f1a1d36673f2765
Dorimanx <yuri@bynet.co.il>
2015-02-10 18:39:08 +0200

    SOUND CONTROL: restore lock check and brake after PA gain change.

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -236,6 +236,11 @@ int snd_hax_reg_access(unsigned int reg)
 		/* Analog Power Amp (PA) */
 		case TAIKO_A_RX_HPH_L_GAIN:
 		case TAIKO_A_RX_HPH_R_GAIN:
+#ifdef CONFIG_MACH_LGE
+			if (lge_snd_ctrl_locked > 0)
+				ret = 0;
+			break;
+#endif
 		case TAIKO_A_RX_HPH_L_STATUS:
 		case TAIKO_A_RX_HPH_R_STATUS:
 #ifdef CONFIG_MACH_LGE
