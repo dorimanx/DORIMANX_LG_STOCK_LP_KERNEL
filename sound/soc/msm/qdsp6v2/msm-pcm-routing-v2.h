8c4bb5b50755963860baf770d8355a06e58da508
Ravi Kumar Alamanda <ralama@codeaurora.org>
2015-02-25 11:31:28 -0800

    ASoC: msm: qdsp6v2: Restrict capture path to LL mode
    ULL mode is not supported in capture path.
    
    Change-Id: I71c42ec9fce8f431c48010c828012d3a3871970f
    Signed-off-by: Ravi Kumar Alamanda <ralama@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.h
+++ sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.h
@@ -156,7 +156,7 @@ enum msm_pcm_routing_event {
  * dspst_id:  DSP audio stream ID
  * stream_type: playback or capture
  */
-void msm_pcm_routing_reg_phy_stream(int fedai_id, int perf_mode, int dspst_id,
+int msm_pcm_routing_reg_phy_stream(int fedai_id, int perf_mode, int dspst_id,
 	int stream_type);
 void msm_pcm_routing_reg_psthr_stream(int fedai_id, int dspst_id,
 		int stream_type);
@@ -166,7 +166,7 @@ struct msm_pcm_routing_evt {
 	void *priv_data;
 };
 
-void msm_pcm_routing_reg_phy_stream_v2(int fedai_id, int perf_mode,
+int msm_pcm_routing_reg_phy_stream_v2(int fedai_id, int perf_mode,
 				       int dspst_id, int stream_type,
 				       struct msm_pcm_routing_evt event_info);
 
