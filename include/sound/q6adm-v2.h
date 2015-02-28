01de9c806724823609b32a1afe16cdd9bc6440f5
Patrick Lai <plai@codeaurora.org>
2015-02-28 10:10:09 -0800

    ASoC: msm: qdsp6v2: separate multi channel map for playback and record
    During concurrency testing scenario which starting multi channel playback
    followed by multi channel recording, it was found that channel map of
    recording is over written by channel map of playback. This results in
    failure to open ADM COPP as wrong channel mapping is being passed.
    
    Separate multi channel map to playback and recording and make explicit
    check to decide on which channel map to use during adm open call.
    
    Change-Id: Ib355b95632f0f20b2062eb38b906c9d17aa827e5
    CRs-fixed: 799164
    Signed-off-by: Patrick Lai <plai@codeaurora.org>
    Signed-off-by: Phani Kumar Uppalapati <phaniu@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/sound/q6adm-v2.h
+++ include/sound/q6adm-v2.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -66,8 +66,8 @@ int adm_get_copp_id(int port_id);
 
 int adm_get_lowlatency_copp_id(int port_id);
 
-void adm_set_multi_ch_map(char *channel_map);
+int adm_set_multi_ch_map(char *channel_map, int path);
 
-void adm_get_multi_ch_map(char *channel_map);
+int adm_get_multi_ch_map(char *channel_map, int path);
 
 #endif /* __Q6_ADM_V2_H__ */
