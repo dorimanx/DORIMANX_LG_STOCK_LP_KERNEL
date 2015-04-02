52d9bda6d62d06cb9ce45b5ca7226970ad8b45bf
imoseyon <imoseyon@gmail.com>
2015-04-02 12:55:53 -0700

    fs/namei.c: fix potential memory leak in path_lookupat
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/namei.c
+++ fs/namei.c
@@ -1813,7 +1813,7 @@ static int path_lookupat(int dfd, const char *name,
 	err = path_init(dfd, name, flags | LOOKUP_PARENT, nd, &base);
 
 	if (unlikely(err))
-		return err;
+		goto out;
 
 	current->total_link_count = 0;
 	err = link_path_walk(name, nd);
@@ -1841,6 +1841,7 @@ static int path_lookupat(int dfd, const char *name,
 		}
 	}
 
+out:
 	if (base)
 		fput(base);
 
