b219d1136cdac0db1cf65193529fb73e6baea950
Dorimanx <yuri@bynet.co.il>
2015-04-06 16:22:06 +0300

    Use Python2 to build kernel. and 4.8 gcc. + fix BT bug.

--- clean-kernel.sh
+++ clean-kernel.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-if [ -e /usr/bin/python3 ]; then
-	rm /usr/bin/python
-	ln -s /usr/bin/python2.7 /usr/bin/python
+PYTHON_CHECK=$(ls -la /usr/bin/python | grep python3 | wc -l);
+PYTHON_WAS_3=0;
+
+if [ "$PYTHON_CHECK" -eq "1" ] && [ -e /usr/bin/python2 ]; then
+	if [ -e /usr/bin/python2 ]; then
+		rm /usr/bin/python
+		ln -s /usr/bin/python2 /usr/bin/python
+		echo "Switched to Python2 for cleaning kernel will switch back when done";
+		PYTHON_WAS_3=1;
+	else
+		echo "You need Python2 to clean this kernel. install and come back."
+		exit 1;
+	fi;
+else
+	echo "Python2 is used! all good, cleaning!";
 fi;
 
 cp -pv .config .config.bkp;
@@ -10,8 +22,7 @@ make ARCH=arm mrproper;
 make clean;
 cp -pv .config.bkp .config;
 
-#resore python3
-if [ -e /usr/bin/python3 ]; then
+if [ "$PYTHON_WAS_3" -eq "1" ]; then
 	rm /usr/bin/python
 	ln -s /usr/bin/python3 /usr/bin/python
 fi;
--- default_build.sh
+++ default_build.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
-export PATH=$PATH:tools/lz4demo
-make ARCH=arm CROSS_COMPILE=/android-kernel/android_source/GOOGLE/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi- g2-open_com-perf_defconfig zImage -j16
+PYTHON_CHECK=$(ls -la /usr/bin/python | grep python3 | wc -l);
+PYTHON_WAS_3=0;
 
+if [ "$PYTHON_CHECK" -eq "1" ] && [ -e /usr/bin/python2 ]; then
+	if [ -e /usr/bin/python2 ]; then
+		rm /usr/bin/python
+		ln -s /usr/bin/python2 /usr/bin/python
+		echo "Switched to Python2 for building kernel will switch back when done";
+		PYTHON_WAS_3=1;
+	else
+		echo "You need Python2 to building this kernel. install and come back."
+		exit 1;
+	fi;
+else
+	echo "Python2 is used! all good, building!";
+fi;
+
+export PATH=$PATH:tools/lz4demo
+make ARCH=arm CROSS_COMPILE=/android-kernel/android_source/GOOGLE/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi- g2-open_com-perf_defconfig zImage -j16
+
+if [ "$PYTHON_WAS_3" -eq "1" ]; then
+	rm /usr/bin/python
+	ln -s /usr/bin/python3 /usr/bin/python
+fi;
