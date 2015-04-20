4e0689803798e38d8cf64d5dd8ea52927bbe2c63
Dorimanx <yuri@bynet.co.il>
2015-04-21 00:25:35 +0300

    KERNEL CPU: merge core_control define for thermal driver.

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -238,4 +238,7 @@ void idle_notifier_register(struct notifier_block *n);
 void idle_notifier_unregister(struct notifier_block *n);
 void idle_notifier_call_chain(unsigned long val);
 
+/* warning shut if msm thermal holding cores off on high temp. */
+extern bool core_control;
+
 #endif /* _LINUX_CPU_H_ */
