af372fc99c3448926dc7d4f7a7bc6796317ff700
Dorimanx <yuri@bynet.co.il>
2015-04-27 17:44:40 +0300

    CPUFREQ: Nightmare gov sync with DEV.

--- drivers/cpufreq/cpufreq_nightmare.c
+++ drivers/cpufreq/cpufreq_nightmare.c
@@ -552,13 +552,13 @@ static int cpufreq_governor_nightmare(struct cpufreq_policy *policy,
 
 		mutex_lock(&nightmare_mutex);
 
-		this_nightmare_cpuinfo->freq_table = cpufreq_frequency_get_table(cpu);
-
 		this_nightmare_cpuinfo->cur_policy = policy;
 
 		this_nightmare_cpuinfo->prev_cpu_wall = ktime_to_us(ktime_get());
 
 		this_nightmare_cpuinfo->prev_cpu_idle = get_cpu_idle_time(cpu, &this_nightmare_cpuinfo->prev_cpu_wall, io_busy);
+
+		this_nightmare_cpuinfo->freq_table = cpufreq_frequency_get_table(cpu);
 
 		nightmare_enable++;
 		/*
