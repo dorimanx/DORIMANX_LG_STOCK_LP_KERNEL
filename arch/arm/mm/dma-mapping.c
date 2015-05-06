315578ae71bd4a95a1d98d4acc0b8787577cf842
Dorimanx <yuri@bynet.co.il>
2015-05-06 11:45:45 +0300

    fix arch/arm/mm/dma-mapping.c after merges.

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -1179,7 +1179,7 @@ static struct page **__iommu_alloc_buffer(struct device *dev, size_t size,
 
 		page = pfn_to_page(pfn);
 
-		__dma_clear_buffer(page, size);
+		__dma_clear_buffer(page, size, NULL);
 
 		for (i = 0; i < count; i++)
 			pages[i] = page + i;
