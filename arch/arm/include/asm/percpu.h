55070fe6693769046e960d598b7ba27d52be1fd0
Dorimanx <yuri@bynet.co.il>
2015-05-10 02:22:18 +0300

    Revert "ARM: 7747/1: pcpu: ensure __my_cpu_offset cannot be re-ordered across barrier()"
    This reverts commit d4e4d5b6017156229a1a05fe6a951493f5c4424a.

--- arch/arm/include/asm/percpu.h
+++ arch/arm/include/asm/percpu.h
@@ -30,15 +30,8 @@ static inline void set_my_cpu_offset(unsigned long off)
 static inline unsigned long __my_cpu_offset(void)
 {
 	unsigned long off;
-	register unsigned long *sp asm ("sp");
-
-	/*
-	 * Read TPIDRPRW.
-	 * We want to allow caching the value, so avoid using volatile and
-	 * instead use a fake stack read to hazard against barrier().
-	 */
-	asm("mrc p15, 0, %0, c13, c0, 4" : "=r" (off) : "Q" (*sp));
-
+	/* Read TPIDRPRW */
+	asm("mrc p15, 0, %0, c13, c0, 4" : "=r" (off) : : "memory");
 	return off;
 }
 #define __my_cpu_offset __my_cpu_offset()
