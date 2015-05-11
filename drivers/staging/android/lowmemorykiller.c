3d98e65f2e176b9e992e7e9b846ef5fc0ca5a454
Dorimanx <yuri@bynet.co.il>
2015-05-11 19:08:48 +0300

    LOWMEMKILL: for now remove protection of systemui and home from OOM.
    with LP, we need more RAM when something big loaded to ram, as game...
    
    so allow OOM service to kill systemui and home and some system services
    that drain more than 80MB RAM.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -110,6 +110,7 @@ static bool avoid_to_kill(uid_t uid)
 	return 0;
 }
 
+#if 0 /* LP draning RAM, We need to trigger OOM on protected_apps for now */
 static bool protected_apps(char *comm)
 {
 	if (strcmp(comm, "d.process.acore") == 0 ||
@@ -120,6 +121,7 @@ static bool protected_apps(char *comm)
 	}
 	return 0;
 }
+#endif
 
 static int test_task_flag(struct task_struct *p, int flag)
 {
@@ -456,8 +458,12 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		pcred = __task_cred(p);
 		uid = pcred->uid;
 
+#if 0 /* LP draning RAM, We need to trigger OOM on protected_apps for now */
 		if (avoid_to_kill(uid) || protected_apps(p->comm)) {
-			if (tasksize * (long)(PAGE_SIZE / 1024) >= 100000) {
+#else
+		if (avoid_to_kill(uid)) {
+#endif
+			if (tasksize * (long)(PAGE_SIZE / 1024) >= 80000) {
 				selected = p;
 				selected_tasksize = tasksize;
 				selected_oom_score_adj = oom_score_adj;
