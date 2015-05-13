4fa2768f0690692ff542afd8785873fcba5df2a6
Dorimanx <yuri@bynet.co.il>
2015-05-13 17:39:27 +0300

    KERNEL INSTALLER: install dorimanx app with kernel. name change..
    Install to priv-app to allow start on boot.
    
    Set permissions. and cleanup.

--- READY-KERNEL/install.sh
+++ READY-KERNEL/install.sh
@@ -1,4 +1,14 @@
 #!/sbin/sh
 
+if [ -e /system/app/STweaks.apk ]; then
+	rm /system/app/STweaks.apk;
+fi;
+if [ -e /system/priv-app/STweaks.apk ]; then
+	rm /system/priv-app/STweaks.apk;
+fi;
+if [ -e /system/app/re.codefi.savoca.kcal-v1.1.apk ]; then
+	rm /system/app/re.codefi.savoca.kcal-v1.1.apk;
+fi;
+
 dd if=/tmp/boot.img of=/dev/block/platform/msm_sdcc.1/by-name/boot || exit 1
 exit 0
