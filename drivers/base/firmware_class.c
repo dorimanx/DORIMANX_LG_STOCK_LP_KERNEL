975b6343ab71c8688d21d00f7e34287118b7e234
Dorimanx <yuri@bynet.co.il>
2015-06-11 17:32:01 +0300

    driver-core: Use kobj_to_dev in all functions in firmware_class.c

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -331,7 +331,7 @@ static ssize_t firmware_direct_read(struct file *filp, struct kobject *kobj,
 				  struct bin_attribute *bin_attr,
 				  char *buffer, loff_t offset, size_t count)
 {
-	struct device *dev = to_dev(kobj);
+	struct device *dev = kobj_to_dev(kobj);
 	struct firmware_priv *fw_priv = to_firmware_priv(dev);
 	struct firmware *fw;
 	ssize_t ret_count;
@@ -443,7 +443,7 @@ static ssize_t firmware_direct_write(struct file *filp, struct kobject *kobj,
 				   struct bin_attribute *bin_attr,
 				   char *buffer, loff_t offset, size_t count)
 {
-	struct device *dev = to_dev(kobj);
+	struct device *dev = kobj_to_dev(kobj);
 	struct firmware_priv *fw_priv = to_firmware_priv(dev);
 	struct firmware *fw;
 	ssize_t retval;
