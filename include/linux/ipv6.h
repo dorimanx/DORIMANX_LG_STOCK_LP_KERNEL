b810698ffec1f7b7c14fcb9cdc2fafb7a20e96c3
Dorimanx <yuri@bynet.co.il>
2015-07-29 10:20:33 +0300

    ipv6: sysctl to restrict candidate source addresses
    Per RFC 6724, section 4, "Candidate Source Addresses":
    
    It is RECOMMENDED that the candidate source addresses be the set
    of unicast addresses assigned to the interface that will be used
    to send to the destination (the "outgoing" interface).
    
    Add a sysctl to enable this behaviour.
    Signed-off-by: Erik Kline <ek@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    [Simplified back-port of net-next 3985e8a3611a93bb36789f65db862e5700aab65e]
    
    Bug: 19470192
    Bug: 21832279
    Bug: 22464419
    
    Change-Id: Icd96382f814a6f3ea53f05beb98c266b1929c5a3
    
    https://www.codeaurora.org/cgit/quic/la/kernel/msm/commit/?h=aosp-common/android-3.4&id=c622347e9f1d4c555f453a01691d57fde700b073

--- include/linux/ipv6.h
+++ include/linux/ipv6.h
@@ -177,6 +177,7 @@ struct ipv6_devconf {
 	__s32		disable_ipv6;
 	__s32		accept_dad;
 	__s32		force_tllao;
+	__s32		use_oif_addrs_only;
 	__s32		accept_ra_prefix_route;
 	void		*sysctl;
 };
@@ -225,6 +226,7 @@ enum {
 	DEVCONF_RA_INFO_FLAG,
 #endif
 	DEVCONF_USE_OPTIMISTIC,
+	DEVCONF_USE_OIF_ADDRS_ONLY,
 	DEVCONF_MAX
 };
 
--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -218,6 +218,7 @@ static struct ipv6_devconf ipv6_devconf __read_mostly = {
 	.accept_source_route	= 0,	/* we do not accept RH0 by default. */
 	.disable_ipv6		= 0,
 	.accept_dad		= 1,
+	.use_oif_addrs_only	= 0,
 	.accept_ra_prefix_route = 1,
 };
 
@@ -257,6 +258,7 @@ static struct ipv6_devconf ipv6_devconf_dflt __read_mostly = {
 	.accept_source_route	= 0,	/* we do not accept RH0 by default. */
 	.disable_ipv6		= 0,
 	.accept_dad		= 1,
+	.use_oif_addrs_only	= 0,
 	.accept_ra_prefix_route = 1,
 };
 
@@ -1195,9 +1197,15 @@ int ipv6_dev_get_saddr(struct net *net, struct net_device *dst_dev,
 		 *    include addresses assigned to interfaces
 		 *    belonging to the same site as the outgoing
 		 *    interface.)
+		 *  - "It is RECOMMENDED that the candidate source addresses
+		 *    be the set of unicast addresses assigned to the
+		 *    interface that will be used to send to the destination
+		 *    (the 'outgoing' interface)." (RFC 6724)
 		 */
+		idev = dst_dev ? __in6_dev_get(dst_dev) : NULL;
 		if (((dst_type & IPV6_ADDR_MULTICAST) ||
-		     dst.scope <= IPV6_ADDR_SCOPE_LINKLOCAL) &&
+		     dst.scope <= IPV6_ADDR_SCOPE_LINKLOCAL ||
+		     (idev && idev->cnf.use_oif_addrs_only)) &&
 		    dst.ifindex && dev->ifindex != dst.ifindex)
 			continue;
 
@@ -4181,6 +4189,7 @@ static inline void ipv6_store_devconf(struct ipv6_devconf *cnf,
 	array[DEVCONF_DISABLE_IPV6] = cnf->disable_ipv6;
 	array[DEVCONF_ACCEPT_DAD] = cnf->accept_dad;
 	array[DEVCONF_FORCE_TLLAO] = cnf->force_tllao;
+	array[DEVCONF_USE_OIF_ADDRS_ONLY] = cnf->use_oif_addrs_only;
 #ifdef CONFIG_LGE_DHCPV6_WIFI
 	array[DEVCONF_RA_INFO_FLAG] = cnf->ra_info_flag;
 #endif	
@@ -4885,6 +4894,14 @@ static struct addrconf_sysctl_table
 			.proc_handler	= proc_dointvec
 		},
 #endif
+		{
+			.procname       = "use_oif_addrs_only",
+			.data           = &ipv6_devconf.use_oif_addrs_only,
+			.maxlen         = sizeof(int),
+			.mode           = 0644,
+			.proc_handler   = proc_dointvec,
+
+		},
 		{
 			/* sentinel */
 		}
