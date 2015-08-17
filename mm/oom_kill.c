a3d26c5f6febab2f1814e0a5d112a6edeb5b7ca5
Dorimanx <yuri@bynet.co.il>
2015-08-17 18:17:27 +0300

    MM: Fixed mm/oom_kill.c and mm/page_io.c

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -428,8 +428,8 @@ void dump_tasks(const struct mem_cgroup *memcg, const nodemask_t *nodemask)
 		}
 
 		pr_info("[%5d] %5d %5d %8lu %8lu %7lu %8lu         %5hd %s\n",
-			task->pid, from_kuid(&init_user_ns, task_uid(task)),
-			task->tgid, task->mm->total_vm, get_mm_rss(task->mm),
+			task->pid, task_uid(task), task->tgid,
+			task->mm->total_vm, get_mm_rss(task->mm),
 			task->mm->nr_ptes,
 			get_mm_counter(task->mm, MM_SWAPENTS),
 			task->signal->oom_score_adj, task->comm);
