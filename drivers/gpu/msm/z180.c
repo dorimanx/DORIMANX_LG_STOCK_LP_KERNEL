c5de1369d24703e297095a28ba23fb12e5ef7fdb
Dorimanx <yuri@bynet.co.il>
2015-08-26 12:11:19 +0300

    msm: kgsl: fix drivers/gpu/msm/z180.c

--- drivers/gpu/msm/z180.c
+++ drivers/gpu/msm/z180.c
@@ -533,8 +533,8 @@ static int z180_ringbuffer_init(struct kgsl_device *device)
 	memset(&z180_dev->ringbuffer, 0, sizeof(struct z180_ringbuffer));
 	z180_dev->ringbuffer.prevctx = Z180_INVALID_CONTEXT;
 	z180_dev->ringbuffer.cmdbufdesc.flags = KGSL_MEMFLAGS_GPUREADONLY;
-	return kgsl_allocate_contiguous(&z180_dev->ringbuffer.cmdbufdesc,
-		Z180_RB_SIZE);
+	return kgsl_allocate_contiguous(device,
+			&z180_dev->ringbuffer.cmdbufdesc, Z180_RB_SIZE);
 }
 
 static void z180_ringbuffer_close(struct kgsl_device *device)
