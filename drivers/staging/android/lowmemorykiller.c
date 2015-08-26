a496f7b488cd6cf85f5906ca24c55781e30c4edf
Dorimanx <yuri@bynet.co.il>
2015-08-26 10:48:25 +0300

    LMK: enabled protection for few critical ROM services.
    protected:
    radio = 1001
    media = 1013
    BT = 1002
    dhcp = 1014
    WIFI = 1010
    NFC = 1027
    GPS = 1021

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -193,7 +193,6 @@ static struct notifier_block lmk_vmpr_nb = {
 	.notifier_call = lmk_vmpressure_notifier,
 };
 
-#if 0 /* LP draning RAM, We need to trigger OOM on protected_apps/system for now */
 static bool avoid_to_kill(uid_t uid)
 {
 	/* 
@@ -202,14 +201,19 @@ static bool avoid_to_kill(uid_t uid)
 	 * uid == 1001 > radio
 	 * uid == 1002 > bluetooth
 	 * uid == 1010 > wifi
+	 * uid == 1013 > media
 	 * uid == 1014 > dhcp
+	 * uid == 1021 > gps
+	 * uid == 1027 > nfc
 	 */
-	if (uid == 0 || uid == 1001 || uid == 1002 || uid == 1010 ||
-			uid == 1014)
+	if (uid == 1001 || uid == 1002 || uid == 1010
+			|| uid == 1013 || uid == 1014
+			|| uid == 1021 || uid == 1027)
 		return 1;
 	return 0;
 }
 
+#if 0 /* LP draning RAM, We need to trigger OOM on protected_apps/system for now */
 static bool protected_apps(char *comm)
 {
 	if (strcmp(comm, "d.process.acore") == 0 ||
@@ -596,6 +600,8 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 
 #if 0 /* LP draning RAM, We need to trigger OOM on protected_apps for now */
 		if (avoid_to_kill(uid) || protected_apps(p->comm)) {
+#else
+		if (avoid_to_kill(uid)) {
 			if (tasksize * (long)(PAGE_SIZE / 1024) >= 80000) {
 				selected = p;
 				selected_tasksize = tasksize;
