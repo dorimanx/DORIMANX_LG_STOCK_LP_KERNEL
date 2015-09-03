336f308a3f756a6369418aeb6078c0f5bd378f04
Dorimanx <yuri@bynet.co.il>
2015-09-03 18:57:15 +0300

    Sound Control: Show default speaker gain value when audio is disabled
    Change-Id: Id705c582490b7688665ce4c654a14f24d7db5967
    
    https://github.com/SlimRoms/kernel_lge_msm8974/commit/5f1f867057069dbd44d13dd05ecc582dff5c03b5
    
    Thanks @savoca

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -565,12 +565,23 @@ static ssize_t mic_gain_store(struct kobject *kobj,
 static ssize_t speaker_gain_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
+#ifndef CONFIG_MACH_LGE
         return sprintf(buf, "%u %u\n",
 			taiko_read(fauxsound_codec_ptr,
 				TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL),
 			taiko_read(fauxsound_codec_ptr,
 				TAIKO_A_CDC_RX7_VOL_CTL_B2_CTL));
+#else
+		unsigned int retl, retr;
 
+		retl = taiko_read(fauxsound_codec_ptr,
+			TAIKO_A_CDC_RX3_VOL_CTL_B2_CTL);
+		retr = taiko_read(fauxsound_codec_ptr,
+			TAIKO_A_CDC_RX4_VOL_CTL_B2_CTL);
+
+		return sprintf(buf, "%u %u\n",
+			retl == 24 ? 0 : retl, retr == 24 ? 0 : retr);
+#endif
 }
 
 static ssize_t speaker_gain_store(struct kobject *kobj,
