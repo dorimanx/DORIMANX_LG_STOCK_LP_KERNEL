383bded95e64b908752e2110eb5d30d47f4e3eff
Dorimanx <yuri@bynet.co.il>
2015-10-07 13:48:53 +0300

    drivers: state_notifier: Prevent racy suspend call
    * Using suspend_defer_time > 0 may cause stacking of
      suspend routines if state_suspend is called more than
      once within suspend_defer_time time frame. Prevent this
      by introducing suspend_in_progress.
    
    Thanks @neobuddy89 :)
    https://github.com/neobuddy89/shamu/commit/f32e6a704d32ec5354af7e2fc4df77aa5c38652b

--- drivers/input/touchscreen/state_notifier.c
+++ drivers/input/touchscreen/state_notifier.c
@@ -15,8 +15,9 @@
 #include <linux/fb.h>
 #include <linux/module.h>
 
-#define DEFAULT_SUSPEND_DEFER_TIME 10
-#define DEFAULT_USE_FB_NOTIFIER 0
+#define DEFAULT_SUSPEND_DEFER_TIME	10
+#define DEFAULT_USE_FB_NOTIFIER		0
+#define STATE_NOTIFIER			"state_notifier"
 
 /*
  * debug = 1 will print all
@@ -40,6 +41,7 @@ module_param_named(suspend_defer_time, suspend_defer_time, uint, 0664);
 static struct delayed_work suspend_work;
 static struct workqueue_struct *susp_wq;
 struct work_struct resume_work;
+static bool suspend_in_progress;
 bool use_fb_notifier = DEFAULT_USE_FB_NOTIFIER;
 module_param_named(use_fb_notifier, use_fb_notifier, bool, 0664);
 
@@ -81,20 +83,24 @@ static void _suspend_work(struct work_struct *work)
 {
 	state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
 	state_suspended = true;
-	dprintk("[STATE NOTIFIER] suspend completed.\n");
+	suspend_in_progress = false;
+	dprintk("%s: suspend completed.\n", STATE_NOTIFIER);
 }
 
 static void _resume_work(struct work_struct *work)
 {
 	state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
 	state_suspended = false;
-	dprintk("[STATE NOTIFIER] resume completed.\n");
+	dprintk("%s: resume completed.\n", STATE_NOTIFIER);
 }
 
 void state_suspend(void)
 {
-	if (state_suspended)
+	dprintk("%s: suspend called.\n", STATE_NOTIFIER);
+	if (state_suspended || suspend_in_progress)
 		return;
+
+	suspend_in_progress = true;
 
 	INIT_DELAYED_WORK(&suspend_work, _suspend_work);
 	queue_delayed_work_on(0, susp_wq, &suspend_work, 
@@ -103,8 +109,10 @@ void state_suspend(void)
 
 void state_resume(void)
 {
+	dprintk("%s: resume called.\n", STATE_NOTIFIER);
 	flush_workqueue(susp_wq);
 	cancel_delayed_work_sync(&suspend_work);
+	suspend_in_progress = false;
 
 	if (state_suspended)
 		queue_work_on(0, susp_wq, &resume_work);
