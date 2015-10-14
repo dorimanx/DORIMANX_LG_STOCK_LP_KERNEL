70856740c5a290913213921d0adc08e94cabc439
Dorimanx <yuri@bynet.co.il>
2015-10-14 11:47:36 +0300

    LMK: correct real time minfree sets that i use, and some tuning.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -77,28 +77,28 @@ static short lowmem_adj[6] = {
 };
 static int lowmem_adj_size = 6;
 static int lowmem_minfree[6] = {
-	 3 *  512,	/* Foreground App: 	6 MB	*/
-	 2 * 1024,	/* Visible App: 	8 MB	*/
-	 4 * 1024,	/* Secondary Server: 	16 MB	*/
+	 4 * 1024,	/* Foreground App: 	16 MB	*/
+	 8 * 1024,	/* Visible App: 	32 MB	*/
+	16 * 1024,	/* Secondary Server: 	65 MB	*/
 	28 * 1024,	/* Hidden App: 		114 MB	*/
-	31 * 1024,	/* Content Provider: 	127 MB	*/
-	34 * 1024,	/* Empty App: 		139 MB	*/
+	45 * 1024,	/* Content Provider: 	184 MB	*/
+	50 * 1024,	/* Empty App: 		204 MB	*/
 };
 static int lowmem_minfree_screen_off[6] = {
-	3 * 512,	/* 6MB */
-	2 * 1024,	/* 8MB */
-	4 * 1024,	/* 16MB */
-	28 * 1024,	/* 114MB */
-	31 * 1024,	/* 127 MB */
-	34 * 1024,	/* 139 MB */
+	 4 * 1024,	/* 16 MB */
+	 8 * 1024,	/* 32 MB */
+	16 * 1024,	/* 65 MB */
+	28 * 1024,	/* 114 MB */
+	45 * 1024,	/* 184 MB */
+	50 * 1024,	/* 204 MB */
 };
 static int lowmem_minfree_screen_on[6] = {
-	3 * 512,	/* 6MB */
-	2 * 1024,	/* 8MB */
-	4 * 1024,	/* 16MB */
-	28 * 1024,	/* 114MB */
-	31 * 1024,	/* 127 MB */
-	34 * 1024,	/* 139 MB */
+	 4 * 1024,	/* 16 MB */
+	 8 * 1024,	/* 32 MB */
+	16 * 1024,	/* 65 MB */
+	28 * 1024,	/* 114 MB */
+	45 * 1024,	/* 184 MB */
+	50 * 1024,	/* 204 MB */
 };
 static int lowmem_minfree_size = 6;
 static int lmk_fast_run = 1;
@@ -126,7 +126,7 @@ module_param_named(enable_adaptive_lmk, enable_adaptive_lmk, int,
  * 90-94. Usually this is a pseudo minfree value, higher than the
  * highest configured value in minfree array.
  */
-static int vmpressure_file_min = 53059;
+static int vmpressure_file_min = 52224; /* 51 * 1024 pages */
 module_param_named(vmpressure_file_min, vmpressure_file_min, int,
 	S_IRUGO | S_IWUSR);
 
@@ -623,7 +623,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		if (avoid_to_kill(uid) || protected_apps(p->comm)) {
 #else
 		if (avoid_to_kill(uid)) {
-			if (tasksize * (long)(PAGE_SIZE / 1024) >= 80000) {
+			if (tasksize * (long)(PAGE_SIZE / 1024) >= 50000) {
 				selected = p;
 				selected_tasksize = tasksize;
 				selected_oom_score_adj = oom_score_adj;
