885b964a9a672deb88f3a85dbbfa156bbb417468
Dorimanx <yuri@bynet.co.il>
2015-10-14 11:50:18 +0300

    Revert " staging: android: lowmemorykiller: Don't count reserved free memory"
    This reverts commit 982823573dbac9e8a3956e618f02826052f3317d.
    
    Dorimanx note:
    
    I have tuned the LMK already. and this change makes it very aggressive
    as it's see that it's always below limit when its not. calc is broken!
    
    <6>lowmemorykiller: Killing 'oid.gentlealarm' (874), adj 1000,
    <6>   to free 61644kB on behalf of 'kswapd0' (101) because
    <6>   cache 273936kB is below limit 204800kB for oom_score_adj 1000
    <6>   Free memory is -69148kB above reserved.
    <6>   Free CMA is 9540kB
    <6>   Total reserve is 105680kB
    <6>   Total free pages is 118392kB
    <6>   Total file cache is 282260kB
    <6>   Slab Reclaimable is 7868kB
    <6>   Slab UnReclaimable is 71996kB
    <6>   Total Slab is 79864kB
    <6>   GFP mask is 0xd0
    
    as free mem - Total reserve is wrong. and should be positive value.
    so it's just keep killing apps with low score.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -503,7 +503,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	if (mutex_lock_interruptible(&scan_mutex) < 0)
 		return 0;
 
-	other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
+	other_free = global_page_state(NR_FREE_PAGES);
 
 	if (global_page_state(NR_SHMEM) + total_swapcache_pages() <
 		global_page_state(NR_FILE_PAGES))
