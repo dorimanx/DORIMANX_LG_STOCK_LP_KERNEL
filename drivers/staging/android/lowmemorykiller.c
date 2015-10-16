a242c7032dd01b4634a0426d7f3c69e46bfd46e3
Dorimanx <yuri@bynet.co.il>
2015-10-16 23:43:32 +0300

    LMK: correct vm pressure description only.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -121,7 +121,7 @@ module_param_named(enable_adaptive_lmk, enable_adaptive_lmk, int,
 
 /*
  * This parameter controls the behaviour of LMK when vmpressure is in
- * the range of 88-94. Adaptive lmk triggers based on number of file
+ * the range of 85-94. Adaptive lmk triggers based on number of file
  * pages wrt vmpressure_file_min, when vmpressure is in the range of
  * 85-94. Usually this is a pseudo minfree value, higher than the
  * highest configured value in minfree array.
