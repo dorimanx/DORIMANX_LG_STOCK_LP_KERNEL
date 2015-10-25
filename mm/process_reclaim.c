c5a71691c50c41326f19c48a88c2203c8c5c1de0
Dorimanx <yuri@bynet.co.il>
2015-10-25 10:25:35 +0200

    MM: process_reclaim: tune to allow to reclame in high pressure too.
    tune max swap pages to reclame in one run. to reduce cpu usage.

--- mm/process_reclaim.c
+++ mm/process_reclaim.c
@@ -35,7 +35,7 @@ module_param_named(enable_process_reclaim, enable_process_reclaim, int,
 	S_IRUGO | S_IWUSR);
 
 /* The max number of pages tried to be reclaimed in a single run */
-int per_swap_size = SWAP_CLUSTER_MAX * 32;
+int per_swap_size = SWAP_CLUSTER_MAX * 16;
 module_param_named(per_swap_size, per_swap_size, int, S_IRUGO | S_IWUSR);
 
 int reclaim_avg_efficiency;
@@ -44,7 +44,7 @@ module_param_named(reclaim_avg_efficiency, reclaim_avg_efficiency,
 
 /* The vmpressure region where process reclaim operates */
 static unsigned long pressure_min = 50;
-static unsigned long pressure_max = 90;
+static unsigned long pressure_max = 100;
 module_param_named(pressure_min, pressure_min, ulong, S_IRUGO | S_IWUSR);
 module_param_named(pressure_max, pressure_max, ulong, S_IRUGO | S_IWUSR);
 
