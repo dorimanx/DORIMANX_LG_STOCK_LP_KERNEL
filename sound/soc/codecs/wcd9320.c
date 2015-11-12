8e6b2101306e5b785a48078291278da7584c1b29
Pranav Vashi <neobuddy89@gmail.com>
2015-11-12 19:33:39 +0530

    ASoC: wcd9320: Add HPH auto pull down support
    * Add support for headphone auto pull down on WCD9320
      to avoid noise while plugging headphone in car stereo.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sound/soc/codecs/wcd9320.c
+++ sound/soc/codecs/wcd9320.c
@@ -7441,11 +7441,31 @@ static enum wcd9xxx_cdc_type taiko_get_cdc_type(void)
 	return WCD9XXX_CDC_TYPE_TAIKO;
 }
 
+static void tomtom_codec_hph_auto_pull_down(struct snd_soc_codec *codec,
+					    bool enable)
+{
+	struct wcd9xxx *taiko_core = dev_get_drvdata(codec->dev->parent);
+
+	if (TAIKO_IS_1_0(taiko_core->version))
+		return;
+
+	dev_dbg(codec->dev, "%s: %s auto pull down\n", __func__,
+			enable ? "enable" : "disable");
+	if (enable) {
+		snd_soc_update_bits(codec, TAIKO_A_RX_HPH_L_TEST, 0x01, 0x01);
+		snd_soc_update_bits(codec, TAIKO_A_RX_HPH_R_TEST, 0x01, 0x01);
+	} else {
+		snd_soc_update_bits(codec, TAIKO_A_RX_HPH_L_TEST, 0x01, 0x00);
+		snd_soc_update_bits(codec, TAIKO_A_RX_HPH_R_TEST, 0x01, 0x00);
+	}
+}
+
 static const struct wcd9xxx_mbhc_cb mbhc_cb = {
 	.get_cdc_type = taiko_get_cdc_type,
 	.setup_zdet = taiko_setup_zdet,
 	.compute_impedance = taiko_compute_impedance,
 	.get_hwdep_fw_cal = taiko_get_hwdep_fw_cal,
+	.hph_auto_pulldown_ctrl = tomtom_codec_hph_auto_pull_down,
 };
 
 static const struct wcd9xxx_mbhc_intr cdc_intr_ids = {
