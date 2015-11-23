f7289a3354d2f8e9d168a67592a5b75878a3ec20
Dorimanx <yuri@bynet.co.il>
2015-11-23 17:44:43 +0200

    Revert "SOUND SOC: fix sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c"
    This reverts commit 573068d6046c855b488543cf4358466c85b411e4.

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -3316,7 +3316,6 @@ static int msm_compr_dec_params_put(struct snd_kcontrol *kcontrol,
 	switch (prtd->codec) {
 	case FORMAT_MP3:
 	case FORMAT_MPEG4_AAC:
-	case FORMAT_FLAC:
 		pr_debug("%s: no runtime parameters for codec: %d\n", __func__,
 			 prtd->codec);
 		break;
