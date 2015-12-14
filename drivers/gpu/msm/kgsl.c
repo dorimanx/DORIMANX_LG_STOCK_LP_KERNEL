3ec1fd529cf3fa81e921005a404b7d8f73d3beff
myfluxi <linflux@arcor.de>
2015-12-14 23:52:13 +0100

    msm: kgsl: Update event workqueue as a high priority thread
    Change-Id: I4ac8713aa7c7268447ea70b400bc25ccf69ecafa

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4395,8 +4395,8 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 				PM_QOS_CPU_DMA_LATENCY,
 				PM_QOS_DEFAULT_VALUE);
 
-
-	device->events_wq = create_workqueue("kgsl-events");
+	device->events_wq = alloc_workqueue("kgsl-events",
+					    WQ_MEM_RECLAIM | WQ_HIGHPRI, 0);
 
 	kgsl_add_event_group(&device->global_events, NULL);
 	kgsl_add_event_group(&device->iommu_events, NULL);
