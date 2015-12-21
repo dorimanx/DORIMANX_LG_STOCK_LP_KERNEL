581afe68bac7d295f5a8c60fa2e67b38f0a4a967
Dorimanx <yuri@bynet.co.il>
2015-12-21 23:07:45 +0200

    THERMAL. use zone 0 thermal sensor for max freq limits by ROM and Kernel.

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -72,9 +72,9 @@ static uint32_t hist_index = 0;
 static struct msm_thermal_data msm_thermal_info;
 
 static struct msm_thermal_data_intelli msm_thermal_info_local = {
-	.sensor_id = 5,
+	.sensor_id = 0,
 	.poll_ms = DEFAULT_POLLING_MS,
-	.limit_temp_degC = 70,
+	.limit_temp_degC = 75,
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
 	.freq_control_mask = 0xf,
