fe5c3b05c66d98bc2c40c469a7ee0b2c258dc312
Dorimanx <yuri@bynet.co.il>
2016-01-06 01:47:35 +0200

    rpm-smd: Raise priority of thread waiting for ack using RT mutex
    Lower priority threads making rpm requests could block higher priority
    threads attempting rpm communication. Raise the priority of the thread
    waiting for ack by acquiring a RT Mutex.
    
    Signed-off-by: Karthik Parsha <kparsha@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    https://github.com/neobuddy89/shamu/commit/2df4667e2320e9185c94716bfa682718dac3b625
    
    +++
     rpm-smd: BUG on RPM ack timeout.
    
    bail out on rpm_wait_for_ack completion timeout as it would have led to
    the system getting deadlocked on a mutex.
    
    Signed-off-by: Sridhar Vashist <svashist@motorola.com>
    Reviewed-on: http://gerrit.mot.com/660383
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    https://github.com/neobuddy89/shamu/commit/bff88516636aee985f6ec1f69f85d2d4f51aea93
    
    ++++
     rpm-smd: Increase ack timeout to 5 sec
    
    * Also add missing BUG() call.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    https://github.com/neobuddy89/shamu/commit/52df18486dc357a39fde1c6237541deaabd9f572

--- arch/arm/mach-msm/rpm-smd.c
+++ arch/arm/mach-msm/rpm-smd.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -23,6 +23,7 @@
 #include <linux/irq.h>
 #include <linux/list.h>
 #include <linux/mutex.h>
+#include <linux/rtmutex.h>
 #include <linux/spinlock.h>
 #include <linux/string.h>
 #include <linux/device.h>
@@ -1205,6 +1206,7 @@ EXPORT_SYMBOL(msm_rpm_send_request_noirq);
 
 int msm_rpm_wait_for_ack(uint32_t msg_id)
 {
+	static DEFINE_RT_MUTEX(msm_rpm_smd_lock);
 	struct msm_rpm_wait_data *elem;
 	int rc = 0;
 
@@ -1223,7 +1225,13 @@ int msm_rpm_wait_for_ack(uint32_t msg_id)
 	if (!elem)
 		return rc;
 
-	wait_for_completion(&elem->ack);
+	rt_mutex_lock(&msm_rpm_smd_lock);
+	if (!wait_for_completion_timeout(&elem->ack, 50*HZ)) {
+		pr_err("%s TIMEOUT msg_id %d\n", __func__, msg_id);
+		BUG();
+	}
+	rt_mutex_unlock(&msm_rpm_smd_lock);
+
 	trace_rpm_ack_recd(0, msg_id);
 
 	rc = elem->errno;
