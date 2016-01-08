7d8b97be66f3324053e12a27507c90439670e395
Dorimanx <yuri@bynet.co.il>
2016-01-08 17:02:06 +0200

    arm: Dump only kernel region and avoid dumping in user mode
    Dump of memory surrounding register values can leak
    kernel information in case of a user level fault.
    Also virt_addr_valid() confirms whether a given virtual
    address linearly maps to a kernel physical address.
    Thus it offers a more exhaustive test for valid kernel
    space then a check for is_vmalloc().
    
    Signed-off-by: Hanumant Singh <hanumant@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -368,10 +368,6 @@ static void show_data(unsigned long addr, int nbytes, const char *name)
 
 static void show_extra_register_data(struct pt_regs *regs, int nbytes)
 {
-	mm_segment_t fs;
-
-	fs = get_fs();
-	set_fs(KERNEL_DS);
 	show_data(regs->ARM_pc - nbytes, nbytes * 2, "PC");
 	show_data(regs->ARM_lr - nbytes, nbytes * 2, "LR");
 	show_data(regs->ARM_sp - nbytes, nbytes * 2, "SP");
@@ -388,7 +384,6 @@ static void show_extra_register_data(struct pt_regs *regs, int nbytes)
 	show_data(regs->ARM_r8 - nbytes, nbytes * 2, "R8");
 	show_data(regs->ARM_r9 - nbytes, nbytes * 2, "R9");
 	show_data(regs->ARM_r10 - nbytes, nbytes * 2, "R10");
-	set_fs(fs);
 }
 
 void __show_regs(struct pt_regs *regs)
@@ -448,7 +443,8 @@ void __show_regs(struct pt_regs *regs)
 	}
 #endif
 
-	show_extra_register_data(regs, 128);
+	if (get_fs() == get_ds())
+		show_extra_register_data(regs, 128);
 }
 
 void show_regs(struct pt_regs * regs)
