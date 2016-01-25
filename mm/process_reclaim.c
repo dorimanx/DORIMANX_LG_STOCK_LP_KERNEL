0b49dec85b96b1429e6659a9ec5ad2a3e08fe3ff
Dorimanx <yuri@bynet.co.il>
2016-01-25 18:05:41 +0200

    mm: process_reclaim: add vm_pressure sysfs update if enabled. + tuning.

--- mm/process_reclaim.c
+++ mm/process_reclaim.c
@@ -43,10 +43,12 @@ module_param_named(reclaim_avg_efficiency, reclaim_avg_efficiency,
 			int, S_IRUGO);
 
 /* The vmpressure region where process reclaim operates */
-static unsigned long pressure_min = 50;
-static unsigned long pressure_max = 90;
+static unsigned long pressure_min = 40;
+static unsigned long pressure_max = 85;
+static unsigned long process_reclaim_pressure = 0;
 module_param_named(pressure_min, pressure_min, ulong, S_IRUGO | S_IWUSR);
 module_param_named(pressure_max, pressure_max, ulong, S_IRUGO | S_IWUSR);
+module_param_named(process_reclaim_pressure, process_reclaim_pressure, ulong, 0444);
 
 /*
  * Scheduling process reclaim workqueue unecessarily
@@ -60,7 +62,7 @@ module_param_named(pressure_max, pressure_max, ulong, S_IRUGO | S_IWUSR);
 static int swap_eff_win = 2;
 module_param_named(swap_eff_win, swap_eff_win, int, S_IRUGO | S_IWUSR);
 
-static int swap_opt_eff = 50;
+static int swap_opt_eff = 40;
 module_param_named(swap_opt_eff, swap_opt_eff, int, S_IRUGO | S_IWUSR);
 
 static atomic_t skip_reclaim = ATOMIC_INIT(0);
@@ -221,6 +223,8 @@ static int vmpressure_notifier(struct notifier_block *nb,
 
 	if (!enable_process_reclaim)
 		return 0;
+
+	process_reclaim_pressure = action;
 
 	if (!current_is_kswapd())
 		return 0;
