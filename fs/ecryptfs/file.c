c0694ccd5ea47aeb97080dc4cfe2f78e9ebf9a36
Dorimanx <yuri@bynet.co.il>
2016-02-08 11:04:28 +0200

    ENCRYPTFS: clean no longer existing check after patch-3.4.106-107

--- fs/ecryptfs/file.c
+++ fs/ecryptfs/file.c
@@ -217,12 +217,6 @@ static int ecryptfs_open(struct inode *inode, struct file *file)
 		/* Policy code enabled in future release */
 		crypt_stat->flags |= (ECRYPTFS_POLICY_APPLIED
 				      | ECRYPTFS_ENCRYPTED);
-#if 1 // FEATURE_SDCARD_ENCRYPTION DEBUG
-if (mount_crypt_stat && (mount_crypt_stat->flags
-                        & ECRYPTFS_DECRYPTION_ONLY)) {
-ecryptfs_printk(KERN_ERR, "%s:%d:: Error decryption_only set\n", __FUNCTION__, __LINE__);
-}
-#endif
 	}
 	mutex_unlock(&crypt_stat->cs_mutex);
 	rc = ecryptfs_get_lower_file(ecryptfs_dentry, inode);
