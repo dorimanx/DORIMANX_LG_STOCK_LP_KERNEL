23667c75edfb54ddf0130a957359348cea300809
Martijn Coenen <maco@google.com>
2016-02-17 10:48:40 +0100

    mm: vmpressure: dynamic window sizing
    The window size used for calculating vm pressure events was
    previously fixed at 512 pages. The window size has a big
    impact on the rate of notifications sent off to userspace,
    in particular when using the "low" level. On machines with
    a lot of memory, the current value is likely excessive. On
    the other hand, if the window size is too big, we might
    delay memory pressure events for too long, especially at
    critical levels of memory pressure.
    
    This patch attempts to address that problem with two changes.
    
    The first change is to calculate the window size
    based on the machine size, quite similar to how the vm
    watermarks are being calculated. This reduces the chance
    of false positives at any pressure level. Using the machine
    size only makes sense on the root cgroup though; for non-root
    cgroups, their hard memory limit is used to calculate the
    window size. If no hard memory limit is set, we fall back to
    the default window size that was previously used.
    
    The second change is based on an idea from Johannes Weiner, to
    only report medium and low pressure levels for every
    X windows that we scan. This reduces the frequency with
    which we report low/medium pressure levels, but at the same
    time will still report critical memory pressure immediately.
    
    Change-Id: Ieffca055b2fb6aa27ae0179e0a588e6fcb173a61
    Signed-off-by: Martijn Coenen <maco@google.com>
    [neo: Adjusted for in-kernel clients to subscribe for events]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/vmpressure.h
+++ include/linux/vmpressure.h
@@ -8,11 +8,40 @@
 #include <linux/types.h>
 #include <linux/cgroup.h>
 
+enum vmpressure_levels {
+	VMPRESSURE_LOW = 0,
+	VMPRESSURE_MEDIUM,
+	VMPRESSURE_CRITICAL,
+	VMPRESSURE_NUM_LEVELS,
+};
+
 struct vmpressure {
+	/*
+	 * The window size is the number of scanned pages before
+	 * we try to analyze scanned/reclaimed ratio. Using small window
+	 * sizes can cause lot of false positives, but too big window size will
+	 * delay the notifications.
+	 *
+	 * In order to reduce the amount of false positives for low and medium
+	 * levels, those levels aren't reported until we've seen multiple
+	 * windows at those respective pressure levels. This makes sure
+	 * sure that we don't delay notifications when encountering critical
+	 * levels of memory pressure, but also don't spam userspace in case
+	 * nothing serious is going on. The number of windows seen at each
+	 * pressure level is kept in nr_windows below.
+	 *
+	 * For the root mem cgroup, the window size is computed based on the
+	 * total amount of pages available in the system. For non-root cgroups,
+	 * we compute the window size based on the hard memory limit, or if
+	 * that is not set, we fall back to the default window size.
+	 */
+	unsigned long window_size;
+	/* The number of windows we've seen each pressure level occur for */
+	unsigned int nr_windows[VMPRESSURE_NUM_LEVELS];
 	unsigned long scanned;
 	unsigned long reclaimed;
 	unsigned long stall;
-	/* The lock is used to keep the scanned/reclaimed above in sync. */
+	/* The lock is used to keep the members above in sync. */
 	struct spinlock sr_lock;
 
 	/* The list of vmpressure_event structs. */
@@ -32,11 +61,13 @@ extern void vmpressure(gfp_t gfp, struct mem_cgroup *memcg,
 extern void vmpressure_prio(gfp_t gfp, struct mem_cgroup *memcg, int prio);
 
 #ifdef CONFIG_MEMCG
-extern void vmpressure_init(struct vmpressure *vmpr);
+extern void vmpressure_init(struct vmpressure *vmpr, bool is_root);
 extern void vmpressure_cleanup(struct vmpressure *vmpr);
 extern struct vmpressure *memcg_to_vmpressure(struct mem_cgroup *memcg);
 extern struct cgroup_subsys_state *vmpressure_to_css(struct vmpressure *vmpr);
 extern struct vmpressure *css_to_vmpressure(struct cgroup_subsys_state *css);
+extern void vmpressure_update_mem_limit(struct mem_cgroup *memcg,
+					unsigned long new_limit);
 extern int vmpressure_register_event(struct cgroup *cg, struct cftype *cft,
 				     struct eventfd_ctx *eventfd,
 				     const char *args);
--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -28,20 +28,21 @@
 #include <linux/slab.h>
 
 /*
- * The window size (vmpressure_win) is the number of scanned pages before
- * we try to analyze scanned/reclaimed ratio. So the window is used as a
- * rate-limit tunable for the "low" level notification, and also for
- * averaging the ratio for medium/critical levels. Using small window
- * sizes can cause lot of false positives, but too big window size will
- * delay the notifications.
- *
- * As the vmscan reclaimer logic works with chunks which are multiple of
- * SWAP_CLUSTER_MAX, it makes sense to use it for the window size as well.
- *
- * TODO: Make the window size depend on machine size, as we do for vmstat
- * thresholds. Currently we set it to 512 pages (2MB for 4KB pages).
+ * The amount of windows we need to see for each pressure level before
+ * reporting an event for that pressure level.
  */
-static const unsigned long vmpressure_win = SWAP_CLUSTER_MAX * 16;
+static const int const vmpressure_windows_needed[] = {
+	[VMPRESSURE_LOW] = 4,
+	[VMPRESSURE_MEDIUM] = 2,
+	[VMPRESSURE_CRITICAL] = 1,
+};
+
+/**
+ * In case we can't compute a window size for a cgroup, because it's
+ * not the root or it doesn't have a limit set, fall back to the
+ * default window size, which is 512 pages (2MB for 4KB pages).
+ */
+static const unsigned long default_window_size = SWAP_CLUSTER_MAX * 16;
 
 /*
  * These thresholds are used when we account memory pressure through
@@ -133,13 +134,6 @@ static struct vmpressure *vmpressure_parent(struct vmpressure *vmpr)
 }
 #endif
 
-enum vmpressure_levels {
-	VMPRESSURE_LOW = 0,
-	VMPRESSURE_MEDIUM,
-	VMPRESSURE_CRITICAL,
-	VMPRESSURE_NUM_LEVELS,
-};
-
 static const char * const vmpressure_str_levels[] = {
 	[VMPRESSURE_LOW] = "low",
 	[VMPRESSURE_MEDIUM] = "medium",
@@ -197,15 +191,10 @@ struct vmpressure_event {
 };
 
 static bool vmpressure_event(struct vmpressure *vmpr,
-			     unsigned long scanned, unsigned long reclaimed)
+			     enum vmpressure_levels level)
 {
 	struct vmpressure_event *ev;
-	enum vmpressure_levels level;
-	unsigned long pressure;
 	bool signalled = false;
-
-	pressure = vmpressure_calc_pressure(scanned, reclaimed);
-	level = vmpressure_level(pressure);
 
 	mutex_lock(&vmpr->events_lock);
 
@@ -226,6 +215,9 @@ static void vmpressure_work_fn(struct work_struct *work)
 	struct vmpressure *vmpr = work_to_vmpressure(work);
 	unsigned long scanned;
 	unsigned long reclaimed;
+	bool report = false;
+	enum vmpressure_levels level;
+	unsigned long pressure;
 
 	spin_lock(&vmpr->sr_lock);
 	/*
@@ -245,10 +237,17 @@ static void vmpressure_work_fn(struct work_struct *work)
 	reclaimed = vmpr->reclaimed;
 	vmpr->scanned = 0;
 	vmpr->reclaimed = 0;
+	pressure = vmpressure_calc_pressure(scanned, reclaimed);
+	level = vmpressure_level(pressure);
+	if (++vmpr->nr_windows[level] == vmpressure_windows_needed[level]) {
+		vmpr->nr_windows[level] = 0;
+		report = true;
+	}
 	spin_unlock(&vmpr->sr_lock);
-
+	if (!report)
+		return;
 	do {
-		if (vmpressure_event(vmpr, scanned, reclaimed))
+		if (vmpressure_event(vmpr, level))
 			break;
 		/*
 		 * If not handled, propagate the event upward into the
@@ -260,6 +259,7 @@ static void vmpressure_work_fn(struct work_struct *work)
 void vmpressure_memcg(gfp_t gfp, struct mem_cgroup *memcg,
 		unsigned long scanned, unsigned long reclaimed)
 {
+	unsigned long window_size;
 	struct vmpressure *vmpr = memcg_to_vmpressure(memcg);
 
 	BUG_ON(!vmpr);
@@ -293,9 +293,10 @@ void vmpressure_memcg(gfp_t gfp, struct mem_cgroup *memcg,
 	vmpr->scanned += scanned;
 	vmpr->reclaimed += reclaimed;
 	scanned = vmpr->scanned;
+	window_size = vmpr->window_size;
 	spin_unlock(&vmpr->sr_lock);
 
-	if (scanned < vmpressure_win)
+	if (scanned < window_size)
 		return;
 	schedule_work(&vmpr->work);
 }
@@ -306,6 +307,7 @@ void vmpressure_global(gfp_t gfp, unsigned long scanned,
 	struct vmpressure *vmpr = &global_vmpressure;
 	unsigned long pressure;
 	unsigned long stall;
+	unsigned long window_size;
 
 	if (!(gfp & (__GFP_HIGHMEM | __GFP_MOVABLE | __GFP_IO | __GFP_FS)))
 		return;
@@ -323,9 +325,10 @@ void vmpressure_global(gfp_t gfp, unsigned long scanned,
 	stall = vmpr->stall;
 	scanned = vmpr->scanned;
 	reclaimed = vmpr->reclaimed;
+	window_size = vmpr->window_size;
 	spin_unlock(&vmpr->sr_lock);
 
-	if (scanned < vmpressure_win)
+	if (scanned < window_size)
 		return;
 
 	spin_lock(&vmpr->sr_lock);
@@ -337,6 +340,34 @@ void vmpressure_global(gfp_t gfp, unsigned long scanned,
 	pressure = vmpressure_calc_pressure(scanned, reclaimed);
 	pressure = vmpressure_account_stall(pressure, stall, scanned);
 	vmpressure_notify(pressure);
+}
+
+static void vmpressure_update_window_size(struct vmpressure *vmpr,
+					  unsigned long total_pages)
+{
+	spin_lock(&vmpr->sr_lock);
+	/*
+	 * This is inspired by the low watermark computation:
+	 * We want a small window size for small machines, but don't
+	 * grow linearly, since users may want to do cache management
+	 * at a finer granularity.
+	 *
+	 * Using sqrt(4 * total_pages) yields the following:
+	 *
+	 * 32MB:	724k
+	 * 64MB:	1024k
+	 * 128MB:	1448k
+	 * 256MB:	2048k
+	 * 512MB:	2896k
+	 * 1024MB:	4096k
+	 * 2048MB:	5792k
+	 * 4096MB:	8192k
+	 * 8192MB:	11584k
+	 * 16384MB:	16384k
+	 * 32768MB:	23170k
+	 */
+	vmpr->window_size = int_sqrt(total_pages * 4);
+	spin_unlock(&vmpr->sr_lock);
 }
 
 /**
@@ -375,6 +406,8 @@ void vmpressure(gfp_t gfp, struct mem_cgroup *memcg,
  */
 void vmpressure_prio(gfp_t gfp, struct mem_cgroup *memcg, int prio)
 {
+	struct vmpressure *vmpr;
+	unsigned long window_size;
 	/*
 	 * We only use prio for accounting critical level. For more info
 	 * see comment for vmpressure_level_critical_prio variable above.
@@ -382,14 +415,40 @@ void vmpressure_prio(gfp_t gfp, struct mem_cgroup *memcg, int prio)
 	if (prio > vmpressure_level_critical_prio)
 		return;
 
+	vmpr = memcg_to_vmpressure(memcg);
+	spin_lock(&vmpr->sr_lock);
+	window_size = vmpr->window_size;
+	spin_unlock(&vmpr->sr_lock);
 	/*
 	 * OK, the prio is below the threshold, updating vmpressure
 	 * information before shrinker dives into long shrinking of long
-	 * range vmscan. Passing scanned = vmpressure_win, reclaimed = 0
+	 * range vmscan. Passing scanned = window_size, reclaimed = 0
 	 * to the vmpressure() basically means that we signal 'critical'
 	 * level.
 	 */
-	vmpressure(gfp, memcg, vmpressure_win, 0);
+	vmpressure(gfp, memcg, window_size, 0);
+}
+
+/**
+ * vmpressure_update_mem_limit() - Lets vmpressure know about a new memory limit
+ * @memcg:	cgroup for which the limit is being updated
+ * @limit:	new limit in pages
+ *
+ * This function lets vmpressure know the memory limit for a specific cgroup
+ * was changed. This allows us to compute new window sizes for this cgroup.
+ */
+void vmpressure_update_mem_limit(struct mem_cgroup *memcg,
+				 unsigned long limit)
+{
+	struct vmpressure *vmpr = memcg_to_vmpressure(memcg);
+
+	/* Clamp to number of pages above the watermark, to avoid creating
+	 * way too large windows when erroneously high limits are set.
+	 */
+	if (limit > nr_free_pagecache_pages())
+		limit = nr_free_pagecache_pages();
+
+	vmpressure_update_window_size(vmpr, limit);
 }
 
 /**
@@ -481,17 +540,28 @@ void vmpressure_unregister_event(struct cgroup *cg, struct cftype *cft,
  * This function should be called on every allocated vmpressure structure
  * before any usage.
  */
-void vmpressure_init(struct vmpressure *vmpr)
+void vmpressure_init(struct vmpressure *vmpr, bool is_root)
 {
 	spin_lock_init(&vmpr->sr_lock);
 	mutex_init(&vmpr->events_lock);
 	INIT_LIST_HEAD(&vmpr->events);
 	INIT_WORK(&vmpr->work, vmpressure_work_fn);
+	if (is_root) {
+		/* For the root mem cgroup, compute the window size
+		 * based on the total amount of memory in the machine.
+		 */
+		vmpressure_update_window_size(vmpr, nr_free_pagecache_pages());
+	} else {
+		/* Use default window size, until a hard limit is set
+		 * on this cgroup.
+		 */
+		vmpr->window_size = default_window_size;
+	}
 }
 
-int vmpressure_global_init(void)
+static int vmpressure_global_init(void)
 {
-	vmpressure_init(&global_vmpressure);
+	vmpressure_init(&global_vmpressure, true);
 	return 0;
 }
 late_initcall(vmpressure_global_init);
