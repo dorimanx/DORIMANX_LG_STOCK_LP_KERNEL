37740f93b0f240bea32500a13444cfa7b6695540
Dorimanx <yuri@bynet.co.il>
2016-03-07 16:16:08 +0200

    AUTOSMP HOTPLUG: add touch core boost! and min_boost_freq and other fixes.
    * Now gov will respond by enabling cores when touch is registered.
    * Gov will turn all cores OFF when stoping activity.
    * Gov will not turn cores ON if cpu0 freq is below min_boost_freq
    to save power, as there is no real load just spikes.
    * Tuned to be more balanced.
    * Added sysfs for touch boost and min boost freq.

--- arch/arm/mach-msm/autosmp.c
+++ arch/arm/mach-msm/autosmp.c
@@ -25,7 +25,9 @@
 #include <linux/workqueue.h>
 #include <linux/cpu.h>
 #include <linux/cpumask.h>
+#include <linux/slab.h>
 #include <linux/hrtimer.h>
+#include <linux/input.h>
 #ifdef CONFIG_STATE_NOTIFIER
 #include <linux/state_notifier.h>
 #endif
@@ -33,8 +35,13 @@
 
 #define DEBUG 0
 
-#define ASMP_TAG "AutoSMP:"
-#define ASMP_STARTDELAY 1000
+#define ASMP_TAG		"AutoSMP:"
+#define ASMP_STARTDELAY		1000
+#define DEFAULT_BOOST_LOCK_DUR	500 * 1000L
+#define DEFAULT_NR_CPUS_BOOSTED	2
+#define DEFAULT_UPDATE_RATE	30
+#define MIN_INPUT_INTERVAL	150 * 1000L
+#define DEFAULT_MIN_BOOST_FREQ	1728000
 
 struct asmp_cpudata_t {
 	long long unsigned int times_hotplugged;
@@ -54,19 +61,29 @@ static struct asmp_param_struct {
 	unsigned int cpufreq_down;
 	unsigned int cycle_up;
 	unsigned int cycle_down;
+	unsigned int cpus_boosted;
+	unsigned int min_boost_freq;
+	unsigned int target_cpus;
+	u64 boost_lock_dur;
+	u64 last_input;
 	struct notifier_block notif;
 	struct mutex autosmp_hotplug_mutex;
+	struct work_struct up_work;
 } asmp_param = {
-	.delay = 30,
+	.delay = DEFAULT_UPDATE_RATE,
 	.suspended = 0,
 	.max_cpus = CONFIG_NR_CPUS,
 	.min_cpus = 1,
-	.cpufreq_up = 90,
-	.cpufreq_down = 60,
+	.cpufreq_up = 95,
+	.cpufreq_down = 80,
 	.cycle_up = 1,
 	.cycle_down = 1,
+	.min_boost_freq = DEFAULT_MIN_BOOST_FREQ,
+	.cpus_boosted = DEFAULT_NR_CPUS_BOOSTED,
+	.boost_lock_dur = DEFAULT_BOOST_LOCK_DUR,
 };
 
+static u64 last_boost_time;
 static unsigned int cycle = 0;
 static int autosmp_enabled __read_mostly = 0;
 static int enable_switch = 0;
@@ -88,7 +105,9 @@ static void __cpuinit asmp_work_fn(struct work_struct *work)
 	unsigned int cpu = 0, slow_cpu = 0;
 	unsigned int rate, cpu0_rate, slow_rate = UINT_MAX, fast_rate;
 	unsigned int max_rate, up_rate, down_rate;
-	int nr_cpu_online;
+	unsigned int nr_cpu_online;
+	unsigned int min_boost_freq = asmp_param.min_boost_freq;
+	u64 now;
 	
 	if (!autosmp_enabled)
 		return;
@@ -98,15 +117,14 @@ static void __cpuinit asmp_work_fn(struct work_struct *work)
 	/* get maximum possible freq for cpu0 and
 	   calculate up/down limits */
 	max_rate  = cpufreq_quick_get_max(cpu);
-	up_rate   = (asmp_param.cpufreq_up * max_rate) / 100;
-	down_rate = (asmp_param.cpufreq_down * max_rate) / 100;
+	up_rate   = (max_rate / 100) * asmp_param.cpufreq_up;
+	down_rate = (max_rate / 100) * asmp_param.cpufreq_down;
 
 	/* find current max and min cpu freq to estimate load */
 	nr_cpu_online = num_online_cpus();
 	cpu0_rate = cpufreq_quick_get(cpu);
 	fast_rate = cpu0_rate;
 
-	get_online_cpus();
 	for_each_online_cpu(cpu) {
 		if (cpu) {
 			rate = cpufreq_quick_get(cpu);
@@ -117,13 +135,15 @@ static void __cpuinit asmp_work_fn(struct work_struct *work)
 				fast_rate = rate;
 		}
 	}
-	put_online_cpus();
 
 	if (cpu0_rate < slow_rate)
 		slow_rate = cpu0_rate;
 
+	if (max_rate <= asmp_param.min_boost_freq)
+		min_boost_freq = max_rate;
+
 	/* hotplug one core if all online cores are over up_rate limit */
-	if (slow_rate > up_rate) {
+	if ((slow_rate > up_rate) && (fast_rate >= min_boost_freq)) {
 		if ((nr_cpu_online < asmp_param.max_cpus) &&
 		    (cycle >= asmp_param.cycle_up)) {
 			cpu = cpumask_next_zero(0, cpu_online_mask);
@@ -138,6 +158,14 @@ static void __cpuinit asmp_work_fn(struct work_struct *work)
 	} else if (slow_cpu && (fast_rate < down_rate)) {
 		if ((nr_cpu_online > asmp_param.min_cpus) &&
 		    (cycle >= asmp_param.cycle_down)) {
+
+			/* check if core touch boosted before cpu_down */
+			now = ktime_to_us(ktime_get());
+			if (nr_cpu_online <= asmp_param.cpus_boosted &&
+					(now - asmp_param.last_input <
+					asmp_param.boost_lock_dur))
+				goto reschedule;
+
  			cpu_down(slow_cpu);
 			cycle = 0;
 #if DEBUG
@@ -148,6 +176,7 @@ static void __cpuinit asmp_work_fn(struct work_struct *work)
 		}
 	} /* else do nothing */
 
+reschedule:
 	reschedule_hotplug_work();
 }
 
@@ -240,6 +269,126 @@ static int state_notifier_callback(struct notifier_block *this,
 }
 #endif
 
+static void __ref cpu_up_work(struct work_struct *work)
+{
+	int cpu;
+	unsigned int target = asmp_param.target_cpus;
+
+	for_each_cpu_not(cpu, cpu_online_mask) {
+		if (target <= num_online_cpus())
+			break;
+		if (cpu == 0)
+			continue;
+		cpu_up(cpu);
+	}
+}
+
+static void online_cpu(unsigned int target)
+{
+	unsigned int online_cpus;
+
+	online_cpus = num_online_cpus();
+
+	/*
+	 * Do not online more CPUs if max_cpus_online reached
+	 * and cancel online task if target already achieved.
+	 */
+	if (target <= online_cpus ||
+			online_cpus >= asmp_param.max_cpus)
+		return;
+
+	asmp_param.target_cpus = target;
+	queue_work_on(0, asmp_workq, &asmp_param.up_work);
+}
+
+static void autosmp_input_event(struct input_handle *handle, unsigned int type,
+				unsigned int code, int value)
+{
+	u64 now;
+
+	if (asmp_param.suspended)
+		return;
+	if (!autosmp_enabled)
+		return;
+
+	now = ktime_to_us(ktime_get());
+	asmp_param.last_input = now;
+	if (now - last_boost_time < MIN_INPUT_INTERVAL)
+		return;
+
+	if (num_online_cpus() >= asmp_param.cpus_boosted ||
+			asmp_param.cpus_boosted <= asmp_param.min_cpus)
+		return;
+
+	online_cpu(asmp_param.cpus_boosted);
+	last_boost_time = ktime_to_us(ktime_get());
+}
+
+static int autosmp_input_connect(struct input_handler *handler,
+				 struct input_dev *dev,
+				 const struct input_device_id *id)
+{
+	struct input_handle *handle;
+	int err;
+
+	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
+	if (!handle)
+		return -ENOMEM;
+
+	handle->dev = dev;
+	handle->handler = handler;
+	handle->name = handler->name;
+
+	err = input_register_handle(handle);
+	if (err)
+		goto err_register;
+
+	err = input_open_device(handle);
+	if (err)
+		goto err_open;
+
+	return 0;
+err_open:
+	input_unregister_handle(handle);
+err_register:
+	kfree(handle);
+	return err;
+}
+
+static void autosmp_input_disconnect(struct input_handle *handle)
+{
+	input_close_device(handle);
+	input_unregister_handle(handle);
+	kfree(handle);
+}
+
+static const struct input_device_id autosmp_ids[] = {
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_EVBIT |
+			 INPUT_DEVICE_ID_MATCH_ABSBIT,
+		.evbit = { BIT_MASK(EV_ABS) },
+		.absbit = { [BIT_WORD(ABS_MT_POSITION_X)] =
+			    BIT_MASK(ABS_MT_POSITION_X) |
+			    BIT_MASK(ABS_MT_POSITION_Y) },
+	}, /* multi-touch touchscreen */
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_KEYBIT |
+			 INPUT_DEVICE_ID_MATCH_ABSBIT,
+		.keybit = { [BIT_WORD(BTN_TOUCH)] = BIT_MASK(BTN_TOUCH) },
+		.absbit = { [BIT_WORD(ABS_X)] =
+			    BIT_MASK(ABS_X) | BIT_MASK(ABS_Y) },
+	}, /* touchpad */
+	{ },
+};
+
+static struct input_handler autosmp_input_handler = {
+	.event		= autosmp_input_event,
+	.connect	= autosmp_input_connect,
+	.disconnect	= autosmp_input_disconnect,
+	.name		= ASMP_TAG,
+	.id_table	= autosmp_ids,
+};
+
 static int hotplug_start(void)
 {
 	int ret = 0;
@@ -255,7 +404,15 @@ static int hotplug_start(void)
 		goto quit;
 	}
 
+	ret = input_register_handler(&autosmp_input_handler);
+	if (ret) {
+		pr_err("%s: Failed to register input handler: %d\n",
+		       ASMP_TAG, ret);
+		goto quit;
+	}
+
 	INIT_DELAYED_WORK(&asmp_work, asmp_work_fn);
+	INIT_WORK(&asmp_param.up_work, cpu_up_work);
 	queue_delayed_work_on(0, asmp_workq, &asmp_work,
 				msecs_to_jiffies(asmp_param.delay));
 	return ret;
@@ -266,9 +423,20 @@ quit:
 
 static void hotplug_stop(void)
 {
+	int cpu;
+
+	input_unregister_handler(&autosmp_input_handler);
 	flush_workqueue(asmp_workq);
+	cancel_work_sync(&asmp_param.up_work);
 	cancel_delayed_work_sync(&asmp_work);
 	destroy_workqueue(asmp_workq);
+
+	/* Put all sibling cores to sleep */
+	for_each_online_cpu(cpu) {
+		if (cpu == 0)
+			continue;
+		cpu_down(cpu);
+	}
 }
 
 static int __cpuinit set_enabled(const char *val, const struct kernel_param *kp)
@@ -355,6 +523,83 @@ store_one(cpufreq_down, cpufreq_down);
 store_one(cycle_up, cycle_up);
 store_one(cycle_down, cycle_down);
 
+static ssize_t show_boost_lock_duration(struct device *dev,
+				        struct device_attribute
+				        *asmp_attributes, char *buf)
+{
+	return sprintf(buf, "%llu\n", div_u64(asmp_param.boost_lock_dur, 1000));
+}
+
+static ssize_t store_boost_lock_duration(struct device *dev,
+					 struct device_attribute
+					 *asmp_attributes, const char *buf,
+					 size_t count)
+{
+	int ret;
+	u64 val;
+
+	ret = sscanf(buf, "%llu", &val);
+	if (ret != 1)
+		return -EINVAL;
+
+	asmp_param.boost_lock_dur = val * 1000;
+
+	return count;
+}
+
+static ssize_t show_cpus_boosted(struct device *dev,
+				 struct device_attribute *asmp_attributes,
+				 char *buf)
+{
+	return sprintf(buf, "%u\n", asmp_param.cpus_boosted);
+}
+
+static ssize_t store_cpus_boosted(struct device *dev,
+				  struct device_attribute *asmp_attributes,
+				  const char *buf, size_t count)
+{
+	int ret;
+	unsigned int val;
+
+	ret = sscanf(buf, "%u", &val);
+	if (ret != 1 || val < 1 || val > CONFIG_NR_CPUS)
+		return -EINVAL;
+
+	asmp_param.cpus_boosted = val;
+
+	return count;
+}
+
+
+static ssize_t show_min_boost_freq(struct device *dev,
+				   struct device_attribute *asmp_attributes,
+				   char *buf)
+{
+	return sprintf(buf, "%u\n", asmp_param.min_boost_freq);
+}
+
+static ssize_t store_min_boost_freq(struct device *dev,
+				    struct device_attribute *asmp_attributes,
+				    const char *buf, size_t count)
+{
+	int ret;
+	unsigned int val;
+
+	ret = sscanf(buf, "%u", &val);
+	if (ret != 1)
+		return -EINVAL;
+
+	asmp_param.min_boost_freq = val;
+
+	return count;
+}
+
+static DEVICE_ATTR(boost_lock_duration, 644, show_boost_lock_duration,
+		   store_boost_lock_duration);
+static DEVICE_ATTR(cpus_boosted, 644, show_cpus_boosted, store_cpus_boosted);
+static DEVICE_ATTR(min_boost_freq, 644, show_min_boost_freq,
+		   store_min_boost_freq);
+
 static struct attribute *asmp_attributes[] = {
 	&delay.attr,
 	&min_cpus.attr,
@@ -363,6 +608,9 @@ static struct attribute *asmp_attributes[] = {
 	&cpufreq_down.attr,
 	&cycle_up.attr,
 	&cycle_down.attr,
+	&dev_attr_boost_lock_duration.attr,
+	&dev_attr_cpus_boosted.attr,
+	&dev_attr_min_boost_freq.attr,
 	NULL
 };
 
