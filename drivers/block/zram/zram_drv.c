98e33ddf76a9453d6494778ae86c2d45ffc7d2e2
Pranav Vashi <neobuddy89@gmail.com>
2016-03-14 16:39:41 +0530

    zram: Default lz4 only if available
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -43,7 +43,11 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
+#ifdef CONFIG_ZRAM_LZ4_COMPRESS
 static const char *default_compressor = "lz4";
+#else
+static const char *default_compressor = "lzo";
+#endif
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
