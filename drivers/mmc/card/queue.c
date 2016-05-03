8b73c6feed2e96e5f06bffaf6c3551922387a369
Dorimanx <yuri@bynet.co.il>
2016-05-03 11:34:42 +0300

    MMC: restore 3 commits that add MMC_REQ_SPECIAL_MASK discard/flush operation.
     mmc: queue: exclude asynchronous transfer for special request
    
    Unlike normal r/w request, special requests(discard, flush) is finished
    with a one-time issue_fn. Request change to mqrq_prev makes unnecessary call.
    
    Change-Id: I00b7c428d760560a808c1fb51409c1fd0708bd81
    Signed-off-by: Seungwon Jeon <tgih.jun@samsung.com>
    Reviewed-by: Konstantin Dorfman <kdorfman@codeaurora.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    [asutoshd@codeaurora.org: fixed merge conflicts]
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commit/21474862deb4c94d4f1229f5fb538150ba7117ef
    
     mmc: queue: use cached attributes of special requests
    
    Special requests like discard and flush would be completed
    and their flags cleared to 0x6b6b6b6b. Hence, refer to the
    saved flags further.
    
    CRs-fixed: 933468
    Change-Id: I1e94c5a743eceabb802ca099c215292d2624eb29
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commit/37b9b21c2ded1fff452fe6a2cdb17666e231a89b
    
     mmc: fix host release issue after discard/flush operation
    
    Under function mmc_blk_issue_rq, relese the host when there
    are no more requests after special request(discard, flush),
    To fix reboot stuck issue.
    
    Change-Id: Ie8d1024a4403ed8f8a3f6a2dfcd3874465b28c3b
    Signed-off-by: Pavan Anamula <pavana@codeaurora.org>
    https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commit/f26ef187a1ff07f14ab40eaa43ffdf10002fa137

--- drivers/mmc/card/queue.c
+++ drivers/mmc/card/queue.c
@@ -28,6 +28,7 @@
 
 #define MMC_QUEUE_BOUNCESZ	65536
 
+#define MMC_REQ_SPECIAL_MASK	(REQ_DISCARD | REQ_FLUSH)
 
 /*
  * Based on benchmark tests the default num of requests to trigger the write
@@ -78,6 +79,7 @@ static int mmc_queue_thread(void *d)
 	do {
 		struct request *req = NULL;
 		struct mmc_queue_req *tmp;
+		unsigned int cmd_flags = 0;
 
 		spin_lock_irq(q->queue_lock);
 		set_current_state(TASK_INTERRUPTIBLE);
@@ -87,12 +89,13 @@ static int mmc_queue_thread(void *d)
 
 		if (req || mq->mqrq_prev->req) {
 			set_current_state(TASK_RUNNING);
+			cmd_flags = req ? req->cmd_flags : 0;
 			mq->issue_fn(mq, req);
 			if (test_bit(MMC_QUEUE_NEW_REQUEST, &mq->flags)) {
 				continue; /* fetch again */
 			} else if (test_bit(MMC_QUEUE_URGENT_REQUEST,
 					&mq->flags) && (mq->mqrq_cur->req &&
-					!(mq->mqrq_cur->req->cmd_flags &
+					!(cmd_flags &
 						MMC_REQ_NOREINSERT_MASK))) {
 				/*
 				 * clean current request when urgent request
@@ -107,7 +110,13 @@ static int mmc_queue_thread(void *d)
 			/*
 			 * Current request becomes previous request
 			 * and vice versa.
+			 * In case of special requests, current request
+			 * has been finished. Do not assign it to previous
+			 * request.
 			 */
+			if (cmd_flags & MMC_REQ_SPECIAL_MASK)
+				mq->mqrq_cur->req = NULL;
+
 			mq->mqrq_prev->brq.mrq.data = NULL;
 			mq->mqrq_prev->req = NULL;
 			tmp = mq->mqrq_prev;
