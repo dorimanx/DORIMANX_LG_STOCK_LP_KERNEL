196edad49bc963be37407a89059b040e9ba27a6d
Thierry Strudel <tstrudel@google.com>
2016-06-14 17:46:44 -0700

    cpu: send KOBJ_ONLINE event when enabling cpus
    In case some sysfs nodes needs to be labeled with a different label than
    sysfs then user needs to be notified when a core is brought back online.
    
    Bug: 29359497
    Change-Id: I0395c86e01cd49c348fda8f93087d26f88557c91
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -599,6 +599,7 @@ static void boost_freq_timer_cb(unsigned long data)
 void __ref enable_nonboot_cpus(void)
 {
 	int cpu, error;
+	struct device *cpu_device;
 #if defined(CONFIG_MACH_MSM8974_B1_KR) || defined(CONFIG_MACH_MSM8974_B1W)
 	static int first = 0;
 	if (!first) {
@@ -629,6 +630,12 @@ void __ref enable_nonboot_cpus(void)
 		error = _cpu_up(cpu, 1);
 		if (!error) {
 			printk(KERN_INFO "CPU%d is up\n", cpu);
+			cpu_device = get_cpu_device(cpu);
+			if (!cpu_device)
+				pr_err("%s: failed to get cpu%d device\n",
+				       __func__, cpu);
+			else
+				kobject_uevent(&cpu_device->kobj, KOBJ_ONLINE);
 			continue;
 		}
 		printk(KERN_WARNING "Error taking CPU%d up: %d\n", cpu, error);
